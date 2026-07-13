ce8c9f34fb1d3c38fea5d759f182cd36d47aebb1
mohammed <mooodo150@gmail.com>
2026-07-13 12:18:52 +0400

    Add Java data types example

---
+++ Java Task 1/src/JavaDataTypes.java
@@ -0,0 +1,35 @@
+public class JavaDataTypes {
+
+    public static void main(String[] args) {
+
+        // Integer data types
+        byte smallNumber = 100;
+        short mediumNumber = 10000;
+        int number = 50000;
+        long bigNumber = 1000000000L;
+
+        // Decimal data types
+        float price = 5.5f;
+        double pi = 3.141592;
+
+        // Character data type
+        char grade = 'A';
+
+        // Boolean data type
+        boolean isJavaEasy = true;
+
+        // String data type
+        String name = "Mohammed";
+
+        // Print all values
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short: " + mediumNumber);
+        System.out.println("Int: " + number);
+        System.out.println("Long: " + bigNumber);
+        System.out.println("Float: " + price);
+        System.out.println("Double: " + pi);
+        System.out.println("Char: " + grade);
+        System.out.println("Boolean: " + isJavaEasy);
+        System.out.println("String: " + name);
+    }
+}

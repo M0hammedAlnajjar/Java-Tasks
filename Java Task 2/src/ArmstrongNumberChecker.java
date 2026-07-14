f2efe62b071d4ee1e7d75204d702e3e94680ec3a
mohammed <mooodo150@gmail.com>
2026-07-14 16:22:24 +0400

    Create a Java class named ArmstrongNumberChecker for Task 327

--- Java Task 2/src/ArmstrongNumberChecker.java
+++ Java Task 2/src/ArmstrongNumberChecker.java
@@ -1,14 +1,47 @@
 public class ArmstrongNumberChecker {
 
     public static void main(String[] args) {
-    int number=0;
-    int originalNumber=0;
-    int digit;
-    int sum;
-    int count ;
 
-    
+        int number = 153;
+        int originalNumber = number;
+        int digit;
+        int sum = 0;
+        int count = 0;
 
 
+        int temp = number;
+        while (temp > 0) {
+            count++;
+            temp = temp / 10;
+        }
+
+
+        number = originalNumber;
+
+        while (number > 0) {
+
+
+            digit = number % 10;
+
+            int power = 1;
+
+
+            for (int i = 1; i <= count; i++) {
+                power = power * digit;
+            }
+
+
+            sum = sum + power;
+
+            number = number / 10;
+        }
+
+        System.out.println("Number: " + originalNumber);
+
+        if (sum == originalNumber) {
+            System.out.println("The number is an Armstrong number.");
+        } else {
+            System.out.println("The number is not an Armstrong number.");
+        }
     }
 }

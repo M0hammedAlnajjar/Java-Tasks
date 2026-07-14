4cd84e8445c3a5c8e1fd51bc319a57cfa38f40d6
mohammed <mooodo150@gmail.com>
2026-07-14 09:46:52 +0400

    Task 319: Palindrome Number Checker done

--- Java Task 2/src/PalindromeNumberChecker.java
+++ Java Task 2/src/PalindromeNumberChecker.java
@@ -1,2 +1,26 @@
 public class PalindromeNumberChecker {
+
+    public static void main(String[] args) {
+        int number;
+        int originalNumber;
+        int reversedNumber = 0;
+        int digit = 0;
+
+        number = 1221;
+        IO.println("Number: " + number);
+
+        originalNumber = number;
+
+        for (; number != 0; number = number / 10) {
+            digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+        }
+
+
+        if (reversedNumber == originalNumber) {
+            IO.println("The number is a palindrome.");
+        } else {
+            IO.println("The number is not a palindrome.");
+        }
+    }
 }

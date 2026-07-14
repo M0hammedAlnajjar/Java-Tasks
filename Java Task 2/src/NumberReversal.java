53103ac9b6c515793dac79e8614d3450859b861b
mohammed <mooodo150@gmail.com>
2026-07-14 09:12:56 +0400

    Complete Task 318: Number Reversal

--- Java Task 2/src/NumberReversal.java
+++ Java Task 2/src/NumberReversal.java
@@ -1,2 +1,31 @@
 public class NumberReversal {
+
+    public static void main(String[] args) {
+
+        int number=12345;
+        int OriginalNumber=number;
+        int reversedNumber=0;
+        int digit ;
+
+
+        while (number!=0){
+            digit = number % 10;
+
+            reversedNumber =reversedNumber * 10+digit;
+            number = number / 10;
+
+
+            IO.println("Original Number:" + OriginalNumber);
+            IO.println("Reversed Number:" + reversedNumber);
+
+
+
+
+
+        }
+
+
+    }
+
+
 }

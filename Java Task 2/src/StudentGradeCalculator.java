0148a414f8bd80a2d9a198f4f6a9daea5c3c105f
mohammed <mooodo150@gmail.com>
2026-07-14 10:57:37 +0400

    Task 320: Student Grade Calculator it is there

--- Java Task 2/src/StudentGradeCalculator.java
+++ Java Task 2/src/StudentGradeCalculator.java
@@ -1,2 +1,37 @@
 public class StudentGradeCalculator {
+
+}
+    public static void main(String[] args) {
+
+    var grade1=95;
+    var grade2=75;
+    var grade3=60;
+    var average=0.0;
+    String status;
+    average = (grade1+grade2+grade3)/ 3.0;
+    IO.println("Average " +average);
+
+        if (average >= 90) {
+            status = "Excellent";
+        }
+        else if (average >= 75) {
+            status = "Good";
+        }
+        else if (average >= 60) {
+            status = "Pass";
+        }
+        else {
+            status = "Fail";
+        }
+
+
+        System.out.println("Grade 1: " + grade1);
+        System.out.println("Grade 2: " + grade2);
+        System.out.println("Grade 3: " + grade3);
+        System.out.println("Average " + average);
+        System.out.println("status: " + status);
+
+
+
+
 }

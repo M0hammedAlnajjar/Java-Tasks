07dbc1e6f9c4b9752ac3005ea56f84bde8eb02a3
mohammed <mooodo150@gmail.com>
2026-07-14 16:07:00 +0400

    Add DigitFrequencyCounter program for Task 326

--- Java Task 2/src/DigitFrequencyCounter.java
+++ Java Task 2/src/DigitFrequencyCounter.java
@@ -1,15 +1,27 @@
 public class DigitFrequencyCounter {
 
-
     public static void main(String[] args) {
 
-        int number=0;
-        int targetDigit=0;
-        int count=0;
+        int number = 122333;
+        int targetDigit = 3;
+        int count = 0;
         int digit;
 
 
+        while (number > 0) {
 
+            digit = number % 10; /
 
+            if (digit == targetDigit) {
+                count++;
+            }
+
+            number = number / 10;
+        }
+
+        // Display output
+        System.out.println("Number: " + number);
+        System.out.println("Target Digit: " + targetDigit);
+        System.out.println("Frequency: " + count);
     }
 }

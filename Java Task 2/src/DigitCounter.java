fb0e8e12b5e99f2958bad4cc46a37fe3ca73cac0
mohammed <mooodo150@gmail.com>
2026-07-14 11:25:23 +0400

    Task 321: Count Digits in a Number it there

--- Java Task 2/src/DigitCounter.java
+++ Java Task 2/src/DigitCounter.java
@@ -1,9 +1,20 @@
 public class DigitCounter {
     public static void main(String[] args) {
 
-        var number=0;
+        var number=123456;
         var count=0;
 
+        var originalNumber = number;
+
+
+
+        for(number=123456; number != 0; number = number / 10){
+
+            count++;
+        }
+
+        IO.println("Number:"+originalNumber);
+        IO.println("Number of digits:"+ count);
     }
 
 }

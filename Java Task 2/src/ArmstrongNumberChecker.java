f7f81bbc008a450c9ac7761e6e9448209906a6eb
mohammed <mooodo150@gmail.com>
2026-07-14 16:11:52 +0400

    Create a Java class named ArmstrongNumberChecker for Task 327

---
+++ Java Task 2/src/ArmstrongNumberChecker.java
@@ -0,0 +1,14 @@
+public class ArmstrongNumberChecker {
+
+    public static void main(String[] args) {
+    int number=0;
+    int originalNumber=0;
+    int digit;
+    int sum;
+    int count ;
+
+    
+
+
+    }
+}

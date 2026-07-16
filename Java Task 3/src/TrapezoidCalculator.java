fb1131aa9c9e4aaf53c99ab90d9d45a11fe94548
mohammed <mooodo150@gmail.com>
2026-07-16 08:40:28 +0400

    Prompt the user to enter the lengths of the two bases, the height, and the two legs

--- Java Task 3/src/TrapezoidCalculator.java
+++ Java Task 3/src/TrapezoidCalculator.java
@@ -2,5 +2,28 @@ public class TrapezoidCalculator {
 
     static void main() {
 
+        double area;
+        double perimeter;
+
+
+        String lengthsInput = IO.readln("enter the lengths  : ");
+        double bases1= Double.parseDouble(lengthsInput);
+
+
+        String lengthsInput2 = IO.readln("enter the lengths  : ");
+        double bases2 = Double.parseDouble(lengthsInput2);
+
+
+        String  heightInput = IO.readln("enter the height  : ");
+        double  height = Double.parseDouble( heightInput);
+
+
+        String  legsInput = IO.readln("enter the legs 1  : ");
+        double  legs = Double.parseDouble( legsInput);
+
+
+        String  legsInput2 = IO.readln("enter the legs 2  : ");
+        double  legs2 = Double.parseDouble( legsInput2);
+
     }
 }

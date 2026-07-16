872f8bde791d407cfc5fd9325aefeea4216664bc
mohammed <mooodo150@gmail.com>
2026-07-16 12:30:45 +0400

    Create a List<String> named students in task 331

--- Java Task 3/src/StudentListManager.java
+++ Java Task 3/src/StudentListManager.java
@@ -1,5 +1,16 @@
 public class StudentListManager {
-    static void main() {
+    public static void main(String[] args) {
 
+        String[] students = new String[3];
+
+        students[0] = IO.readln("Enter student name: ");
+        students[1] = IO.readln("Enter student name: ");
+        students[2] = IO.readln("Enter student name: ");
+
+        System.out.println("Student List:");
+
+        for (String student : students) {
+            System.out.println(student);
+        }
     }
 }

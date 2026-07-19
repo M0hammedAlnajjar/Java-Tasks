f924b58793e228fa8dc1147c62c3b623a26b43de
mohammed <mooodo150@gmail.com>
2026-07-19 08:10:00 +0400

    Task 331: Student List Manager it is done 100%

--- Java Task 3/src/StudentListManager.java
+++ Java Task 3/src/StudentListManager.java
@@ -12,5 +12,53 @@ public class StudentListManager {
         for (String student : students) {
             System.out.println(student);
         }
+
+        IO.println("Total number of students: " + students.length);
+
+
+        IO.println("First student: " + students[0]);
+
+
+        IO.println("Last student: " + students[students.length - 1]);
+
+
+
+        String searchStudent = IO.readln("Enter a student name to search: ");
+
+        boolean found = false;
+
+        for (String student : students) {
+            if (student.equalsIgnoreCase(searchStudent)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            IO.println(searchStudent + " is in the list.");
+        } else {
+            IO.println(searchStudent + " is not in the list.");
+        }
+
+
+        String studentToRemove = IO.readln("Enter the student name to remove: ");
+
+        boolean removed = false;
+
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equalsIgnoreCase(studentToRemove)) {
+                students[i] = "";
+                removed = true;
+                break;
+            }
+        }
+
+        if (removed) {
+            IO.println(studentToRemove + " has been removed.");
+        } else {
+            IO.println(studentToRemove + " was not found.");
+        }
+
+
     }
 }

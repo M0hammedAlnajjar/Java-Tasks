d9909b2952fe5fa8fbe4a945bcb4a444e47e141e
mohammed <mooodo150@gmail.com>
2026-07-19 09:00:37 +0400

    Add at least 20 integer values to the list, including positive numbers, negative

--- Java Task 3/src/NumberListStatistics.java
+++ Java Task 3/src/NumberListStatistics.java
@@ -1,5 +1,21 @@
-public class NumberListStatistics {
-    static void main() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class NumberListStatistics {
+    public static void main(String[] args) {
+        String valueInput;
+        int value;
+
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+             valueInput = IO.readln("Enter number " + (i + 1) + ": ");
+             value = Integer.parseInt(valueInput);
+
+            numbers.add(value);
+        }
+
+        IO.println("Numbers in the list: " + numbers);
     }
 }

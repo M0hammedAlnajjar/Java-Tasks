38bd2b8f40e99c402ee2a5246ac83a6533a936f4
mohammed <mooodo150@gmail.com>
2026-07-20 09:46:22 +0400

    Store student information and subject marks using variables

--- Java Task 3/src/StudentResultAnalyzer.java
+++ Java Task 3/src/StudentResultAnalyzer.java
@@ -3,14 +3,21 @@ public class StudentResultAnalyzer {
     public static void main(String[] args) {
 
 
-        String studentName;
-        double mathMark;
-        double englishMark;
-        double scienceMark;
+        String studentName = "MOhammed";
+        double mathMark=89;
+        double englishMark=100;
+        double scienceMark=85;
         double totalMarks;
         double averageMark;
         String grade;
         String resultStatus;
+
+
+
+        IO.println("Student Name: " + studentName);
+        IO.println("Math Mark: " + mathMark);
+        IO.println("English Mark: " + englishMark);
+        IO.println("Science Mark: " + scienceMark);
 
     }
 }

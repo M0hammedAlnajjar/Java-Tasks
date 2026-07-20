8ee609c33cae3d31ed2461c3fb7c5c91feba10ff
mohammed <mooodo150@gmail.com>
2026-07-20 09:42:57 +0400

    I  declare all required variables needed for the program using meaningful

--- Java Task 3/src/StudentResultAnalyzer.java
+++ Java Task 3/src/StudentResultAnalyzer.java
@@ -1,5 +1,16 @@
 public class StudentResultAnalyzer {
-    static void main() {
+
+    public static void main(String[] args) {
+
+
+        String studentName;
+        double mathMark;
+        double englishMark;
+        double scienceMark;
+        double totalMarks;
+        double averageMark;
+        String grade;
+        String resultStatus;
 
     }
 }

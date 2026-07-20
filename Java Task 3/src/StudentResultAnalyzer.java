5633045ef741838e9a09f4e69422ada5d86322d1
mohammed <mooodo150@gmail.com>
2026-07-20 10:51:07 +0400

    calculateTotal last update

--- Java Task 3/src/StudentResultAnalyzer.java
+++ Java Task 3/src/StudentResultAnalyzer.java
@@ -3,15 +3,16 @@ public class StudentResultAnalyzer {
     public static void main(String[] args) {
 
 
-        String studentName = "MOhammed";
-        double mathMark=89;
-        double englishMark=100;
-        double scienceMark=85;
+        String studentName = "Mohammed";
+        double mathMark = 89;
+        double englishMark = 100;
+        double scienceMark = 85;
+
+
         double totalMarks;
         double averageMark;
         String grade;
         String resultStatus;
-
 
 
         IO.println("Student Name: " + studentName);
@@ -20,19 +21,31 @@ public class StudentResultAnalyzer {
         IO.println("Science Mark: " + scienceMark);
 
 
+        IO.println("Math mark valid: " + validateMarks(mathMark));
+        IO.println("English mark valid: " + validateMarks(englishMark));
+        IO.println("Science mark valid: " + validateMarks(scienceMark));
 
 
+        totalMarks = calculateTotalMarks(mathMark, englishMark, scienceMark);
+
+        IO.println("Total Marks: " + totalMarks);
     }
 
-    public static void validateMarks(double  mark) {
 
-        if (mark>=0 && mark<=100) {
-            IO.println("true");
 
-        }else{
-            IO.println(" false");
+    public static String validateMarks(double mark) {
 
-        }
+        return (mark >= 0 && mark <= 100) ? "Valid Mark" : "Invalid Mark";
+    }
 
+
+
+    public static double calculateTotalMarks(double mathMark, double englishMark, double scienceMark) {
+
+        double totalMarks;
+
+        totalMarks = mathMark + englishMark + scienceMark;
+
+        return totalMarks;
     }
 }

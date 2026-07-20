2032ebfcb9f6ced0e4673dfbb9da962dc6427596
mohammed <mooodo150@gmail.com>
2026-07-20 09:03:39 +0400

    Use user input in task

--- Java Task 3/src/EmployeeSalaryCalculator.java
+++ Java Task 3/src/EmployeeSalaryCalculator.java
@@ -1,17 +1,28 @@
 public class EmployeeSalaryCalculator {
 
-
-    static void main() {
-
-        double EmployeeName;
-        double BasicSalary;
-        double BonusAmount;
-        double DeductionAmount;
-        double NetSalary;
+    public static void main(String[] args) {
 
 
+        String employeeName;
+        double basicSalary;
+        double bonusAmount;
+        double deductionAmount;
+        double netSalary;
 
 
+        employeeName = IO.readln("Enter employee name: ");
+        basicSalary = Double.parseDouble(IO.readln("Enter basic salary: "));
+        bonusAmount = Double.parseDouble(IO.readln("Enter bonus amount: "));
+        deductionAmount = Double.parseDouble(IO.readln("Enter deduction amount: "));
 
+
+        netSalary = basicSalary + bonusAmount - deductionAmount;
+
+        IO.println("\nEmployee Salary Details");
+        IO.println("Employee Name: " + employeeName);
+        IO.println("Basic Salary: " + basicSalary);
+        IO.println("Bonus Amount: " + bonusAmount);
+        IO.println("Deduction Amount: " + deductionAmount);
+        IO.println("Net Salary: " + netSalary);
     }
 }

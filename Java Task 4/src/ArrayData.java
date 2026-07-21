0b9ca896dbe6b0c9e1fc5abddf033e538823802d
mohammed <mooodo150@gmail.com>
2026-07-21 08:42:17 +0400

    3. Array Statistics

--- Java Task 4/src/ArrayData.java
+++ Java Task 4/src/ArrayData.java
@@ -3,6 +3,12 @@ public class ArrayData {
     static void main() {
 
 
+        int sum=1;
+        int max = numbers[0];
+        int min = numbers[0];
+
+
+
         int[] numbers = {45, 12, -5, 90, 33, 12, 0, 67, -20, 88, 45, 100, 7, -3, 55};
         IO.println("Array Elements:");
 
@@ -19,6 +25,28 @@ public class ArrayData {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Index " + i + " = " + numbers[i]);
         }
+
+// Loop through the array
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+
+        double average = (double) sum / numbers.length;
+
+// Display results
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+        System.out.println("Maximum = " + max);
+        System.out.println("Minimum = " + min);
 
     }
 }

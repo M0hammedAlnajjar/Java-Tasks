378c71fbc6ea3e11872e37fd3d87fdd227d77a20
mohammed <mooodo150@gmail.com>
2026-07-21 10:50:44 +0400

    added

---
+++ Java Task 4/src/StringDemoJava.java
@@ -0,0 +1,11 @@
+public class StringDemoJava {
+
+    public static void main(String[] args) {
+
+        String greeting = "Hello";
+
+        String upperCase = greeting.toUpperCase();
+
+        System.out.println(upperCase);
+    }
+}

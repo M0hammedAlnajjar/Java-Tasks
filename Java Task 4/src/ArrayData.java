6c53018d9388a7e4ad7df61b0e1c5f0853910135
mohammed <mooodo150@gmail.com>
2026-07-21 08:04:36 +0400

    1. Create and Initialize Array

--- Java Task 4/src/ArrayData.java
+++ Java Task 4/src/ArrayData.java
@@ -2,5 +2,12 @@ public class ArrayData {
 
     static void main() {
 
+
+        int[] numbers = {45, 12, -5, 90, 33, 12, 0, 67, -20, 88, 45, 100, 7, -3, 55};
+        IO.println("Array Elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
     }
 }

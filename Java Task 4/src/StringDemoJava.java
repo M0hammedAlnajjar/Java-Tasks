6ea45e5c807b01a0d2e26b7cfd175dcc55e5262d
mohammed <mooodo150@gmail.com>
2026-07-21 11:46:22 +0400

    Find position → indexOf(), lastIndexOf()

--- Java Task 4/src/StringDemoJava.java
+++ Java Task 4/src/StringDemoJava.java
@@ -1,43 +1,50 @@
 public class StringDemoJava {
 
     public static void main(String[] args) {
+//
+//        String greeting = "Hello";
+//        String callName = " Ali ";
+//        String A1 = "Hello Codeline ";
+//        String A2 = "  Hello Codeline  ";
+//        String A3 = "     ";
+//
+//        String upperCase = greeting.toUpperCase();
+//        IO.println(upperCase);
+//
+//        String upperCase1 = callName.toUpperCase();
+//        IO.println("UpperCase it is : " + upperCase1);
+//
+//        int lenSort = A1.length();
+//        IO.println("The length it is : " + lenSort);
+//
+//        int lenSor2 = A2.strip().length();
+//        IO.println("Print without spaces length: " + lenSor2);
+//
+//        boolean lenSo3 = (A3.strip().isEmpty());
+//        IO.println("Is empty after strip: " + lenSo3);
+//
+//        boolean lenSo4 = (A3.strip().isBlank());
+//        IO.println("Is blank after strip: " + lenSo4);
+//
+//
+//        String input=IO.readln("Enter your name is :").strip();
+//        if (input.isBlank()){
+//            IO.println("Nmae is reqwied ");
+//        }
+//
+//        String email =IO.readln("Enter your eamil : ").strip();
+//        IO.println(email.indexOf("@"));
+//        IO.println(email.indexOf("$"));
+//
+//
+        String t = "Hello Codeline World";
 
-        String greeting = "Hello";
-        String callName = " Ali ";
-        String A1 = "Hello Codeline ";
-        String A2 = "  Hello Codeline  ";
-        String A3 = "     ";
-
-        String upperCase = greeting.toUpperCase();
-        IO.println(upperCase);
-
-        String upperCase1 = callName.toUpperCase();
-        IO.println("UpperCase it is : " + upperCase1);
-
-        int lenSort = A1.length();
-        IO.println("The length it is : " + lenSort);
-
-        int lenSor2 = A2.strip().length();
-        IO.println("Print without spaces length: " + lenSor2);
-
-        boolean lenSo3 = (A3.strip().isEmpty());
-        IO.println("Is empty after strip: " + lenSo3);
-
-        boolean lenSo4 = (A3.strip().isBlank());
-        IO.println("Is blank after strip: " + lenSo4);
-
-
-        String input=IO.readln("Enter your name is :").strip();
-        if (input.isBlank()){
-            IO.println("Nmae is reqwied ");
-        }
-
-        String email =IO.readln("Enter your eamil : ").strip();
-        IO.println(email.indexOf("@"));
-        IO.println(email.indexOf("$"));
-
-
-
+        IO.println(t.indexOf("Codeline"));
+        IO.println(t.indexOf("Java"));
+        IO.println(t.lastIndexOf("Wo"));
+        IO.println(t.contains("World"));
+        IO.println(t.startsWith("Hello"));
+        IO.println(t.endsWith("World"));
 
 
 

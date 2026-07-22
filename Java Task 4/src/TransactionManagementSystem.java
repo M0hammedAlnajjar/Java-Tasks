e2d950f08b19b337d02510622c18afadf848720e
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-22 22:51:59 +0400

    Fix it Deposit + Withdraw + Transfer

--- Java Task 4/src/TransactionManagementSystem.java
+++ Java Task 4/src/TransactionManagementSystem.java
@@ -160,3 +160,266 @@ public class TransactionManagementSystem {
 
         }
 
+        // 3. Deposit Money System
+
+        System.out.println("\n===== Deposit Money =====");
+
+
+        String depositInput =
+                IO.readln("Enter Account Number: ");
+
+        int depositAccountNumber =
+                Integer.parseInt(depositInput);
+
+
+        boolean depositFound = false;
+
+
+
+        for(int i = 0; i < accountNumbers.size(); i++){
+
+
+            if(accountNumbers.get(i).equals(depositAccountNumber)){
+
+
+                depositFound = true;
+
+
+                String amountInput =
+                        IO.readln("Enter Deposit Amount: ");
+
+                double depositAmount =
+                        Double.parseDouble(amountInput);
+
+
+
+                if(depositAmount <= 0){
+
+                    System.out.println("Error: Amount must be greater than zero.");
+
+                }
+
+                else{
+
+
+                    double updatedBalance =
+                            balances.get(i) + depositAmount;
+
+
+                    balances.set(i, updatedBalance);
+
+
+                    System.out.println("Deposit successful.");
+
+                    System.out.println("New Balance: "
+                            + updatedBalance);
+
+                }
+
+
+                break;
+
+            }
+
+        }
+
+
+
+        if(!depositFound){
+
+            System.out.println("Account not found.");
+
+        }
+
+
+
+// 4. Withdraw Money System
+
+
+        System.out.println("\n===== Withdraw Money =====");
+
+
+        String withdrawInput =
+                IO.readln("Enter Account Number: ");
+
+        int withdrawAccountNumber =
+                Integer.parseInt(withdrawInput);
+
+
+
+        boolean withdrawFound = false;
+
+
+
+        for(int i = 0; i < accountNumbers.size(); i++){
+
+
+            if(accountNumbers.get(i).equals(withdrawAccountNumber)){
+
+
+                withdrawFound = true;
+
+
+
+                String amountInput =
+                        IO.readln("Enter Withdrawal Amount: ");
+
+
+                double withdrawAmount =
+                        Double.parseDouble(amountInput);
+
+
+
+                if(withdrawAmount <= 0){
+
+                    System.out.println("Error: Amount must be greater than zero.");
+
+                }
+
+                else if(withdrawAmount > balances.get(i)){
+
+
+                    System.out.println("Error: Insufficient balance.");
+
+                }
+
+                else{
+
+
+                    double newBalance =
+                            balances.get(i) - withdrawAmount;
+
+
+                    balances.set(i,newBalance);
+
+
+                    System.out.println("Withdrawal successful.");
+
+                    System.out.println("Remaining Balance: "
+                            + newBalance);
+
+                }
+
+
+                break;
+
+            }
+
+        }
+
+
+
+        if(!withdrawFound){
+
+            System.out.println("Account not found.");
+
+        }
+
+
+
+// 5. Transfer Money System
+
+
+        System.out.println("\n===== Transfer Money =====");
+
+
+        int senderIndex = -1;
+        int receiverIndex = -1;
+
+
+
+        int senderAccount =
+                Integer.parseInt(
+                        IO.readln("Enter Sender Account Number: ")
+                );
+
+
+        int receiverAccount =
+                Integer.parseInt(
+                        IO.readln("Enter Receiver Account Number: ")
+                );
+
+
+        double transferAmount =
+                Double.parseDouble(
+                        IO.readln("Enter Transfer Amount: ")
+                );
+
+
+
+
+        for(int i = 0; i < accountNumbers.size(); i++){
+
+
+            if(accountNumbers.get(i).equals(senderAccount)){
+
+                senderIndex = i;
+
+            }
+
+
+            if(accountNumbers.get(i).equals(receiverAccount)){
+
+                receiverIndex = i;
+
+            }
+
+        }
+
+
+
+        if(senderIndex == -1 || receiverIndex == -1){
+
+
+            System.out.println("Error: Account not found.");
+
+        }
+
+
+        else if(transferAmount <= 0){
+
+
+            System.out.println("Error: Invalid amount.");
+
+        }
+
+
+        else if(balances.get(senderIndex) < transferAmount){
+
+
+            System.out.println("Error: Insufficient balance.");
+
+        }
+
+
+        else{
+
+
+            balances.set(
+                    senderIndex,
+                    balances.get(senderIndex) - transferAmount
+            );
+
+
+            balances.set(
+                    receiverIndex,
+                    balances.get(receiverIndex) + transferAmount
+            );
+
+
+
+            System.out.println("Transfer completed successfully.");
+
+            System.out.println(
+                    "Sender Balance: "
+                            + balances.get(senderIndex)
+            );
+
+
+            System.out.println(
+                    "Receiver Balance: "
+                            + balances.get(receiverIndex)
+            );
+
+        }
+

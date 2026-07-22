6ad24342e359ed95d87b4d044f7e957dfc918c65
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-22 17:45:31 +0400

    2. Display Book Information

--- Java Task 4/src/BookManagementSystem.java
+++ Java Task 4/src/BookManagementSystem.java
@@ -1,97 +1,106 @@
 import java.util.ArrayList;
+
 public class BookManagementSystem {
 
-        public class LibraryBookManagementSystem {
-
-            public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-                // 1. Create Book Lists
+        // 1. Create Book Lists
 
-                ArrayList<String> bookTitles = new ArrayList<>();
+        ArrayList<String> bookTitles = new ArrayList<>();
 
-                ArrayList<String> authors = new ArrayList<>();
+        ArrayList<String> authors = new ArrayList<>();
 
-                ArrayList<Boolean> availability = new ArrayList<>();
+        ArrayList<Boolean> availability = new ArrayList<>();
 
 
 
-                // Add 10 Books
+        // Add 10 Books
 
-                bookTitles.add("Java Programming");
-                authors.add("James Gosling");
-                availability.add(true);
-
-
-                bookTitles.add("Clean Code");
-                authors.add("Robert Martin");
-                availability.add(false);
+        bookTitles.add("Java Programming");
+        authors.add("James Gosling");
+        availability.add(true);
 
 
-                bookTitles.add("Python Basics");
-                authors.add("Mark Lutz");
-                availability.add(true);
+        bookTitles.add("Clean Code");
+        authors.add("Robert Martin");
+        availability.add(false);
 
 
-                bookTitles.add("Database Systems");
-                authors.add("Thomas Connolly");
-                availability.add(true);
+        bookTitles.add("Python Basics");
+        authors.add("Mark Lutz");
+        availability.add(true);
 
 
-                bookTitles.add("Artificial Intelligence");
-                authors.add("Stuart Russell");
-                availability.add(false);
+        bookTitles.add("Database Systems");
+        authors.add("Thomas Connolly");
+        availability.add(true);
 
 
-                bookTitles.add("Web Development");
-                authors.add("Jon Duckett");
-                availability.add(true);
+        bookTitles.add("Artificial Intelligence");
+        authors.add("Stuart Russell");
+        availability.add(false);
 
 
-                bookTitles.add("Computer Networks");
-                authors.add("Andrew Tanenbaum");
-                availability.add(false);
+        bookTitles.add("Web Development");
+        authors.add("Jon Duckett");
+        availability.add(true);
 
 
-                bookTitles.add("Software Engineering");
-                authors.add("Ian Sommerville");
-                availability.add(true);
+        bookTitles.add("Computer Networks");
+        authors.add("Andrew Tanenbaum");
+        availability.add(false);
 
 
-                bookTitles.add("Data Structures");
-                authors.add("Mark Weiss");
-                availability.add(true);
+        bookTitles.add("Software Engineering");
+        authors.add("Ian Sommerville");
+        availability.add(true);
 
 
-                bookTitles.add("Algorithms");
-                authors.add("Robert Sedgewick");
-                availability.add(false);
+        bookTitles.add("Data Structures");
+        authors.add("Mark Weiss");
+        availability.add(true);
+
+
+        bookTitles.add("Algorithms");
+        authors.add("Robert Sedgewick");
+        availability.add(false);
 
 
 
-                // Display Books
+        // 2. Display Book Information
 
-                System.out.println("===== Library Books =====");
-
-
-                for (int i = 0; i < bookTitles.size(); i++) {
+        System.out.println("===== Library Books =====");
 
 
-                    System.out.println("Book " + (i + 1));
+        for (int i = 0; i < bookTitles.size(); i++) {
 
-                    System.out.println("Title: " + bookTitles.get(i));
 
-                    System.out.println("Author: " + authors.get(i));
+            System.out.println("\nBook Index: " + i);
 
-                    System.out.println("Available: " + availability.get(i));
+            System.out.println("Title: " + bookTitles.get(i));
 
-                    System.out.println("---------------------");
+            System.out.println("Author: " + authors.get(i));
 
-                }
 
+            if (availability.get(i)) {
+
+                System.out.println("Status: Available");
+
+            } else {
+
+                System.out.println("Status: Borrowed");
 
             }
+
+
+            System.out.println("---------------------");
+
         }
 
-    }
 
+        System.out.println("\nTotal Books: " + bookTitles.size());
+
+
+    }
+}

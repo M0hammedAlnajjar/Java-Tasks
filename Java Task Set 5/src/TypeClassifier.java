0870701b7b1c63a8f69b756aad3faecc99073a3f
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-23 16:33:36 +0400

    feat: add file names list with different extensions

--- Java Task Set 5/src/TypeClassifier.java
+++ Java Task Set 5/src/TypeClassifier.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TypeClassifier {
+
+    static void main() {
+
+        List<String> fileNames = new ArrayList<>();
+
+        fileNames.add("report.pdf");
+        fileNames.add("photo.jpg");
+        fileNames.add("music.mp3");
+        fileNames.add("video.mp4");
+        fileNames.add("document.docx");
+        fileNames.add("data.csv");
+        fileNames.add("program.java");
+        fileNames.add("README"); // No extension
+
+        System.out.println(fileNames);
+
+    }
 }

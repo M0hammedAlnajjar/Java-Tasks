4f23a696f51182639db570fc4a40e140d22419e1
mohammed <mooodo150@gmail.com>
2026-07-23 10:59:46 +0400

    Read a file name from the user with a Scanner (for example, report.final.pdf)

--- Java Task Set 5/src/NameAnalyzer.java
+++ Java Task Set 5/src/NameAnalyzer.java
@@ -1,6 +1,16 @@
 public class NameAnalyzer {
-
     static void main() {
 
+        String FileName = IO.readln("Enter the file name: ");
+
+        if (FileName.isEmpty() ||
+                FileName.charAt(0) == '.' ||
+                FileName.charAt(FileName.length() - 1) == '.') {
+
+            IO.println("Invalid file name");
+            return;
+        }
+
+        IO.println("Valid file name.");
     }
 }

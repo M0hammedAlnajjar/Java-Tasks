abe474c0eb2dfe7b82223bb7dfbbb00634dccdeb
mohammed <mooodo150@gmail.com>
2026-07-23 11:19:19 +0400

    Classify each extension

--- Java Task Set 5/src/NameAnalyzer.java
+++ Java Task Set 5/src/NameAnalyzer.java
@@ -10,7 +10,31 @@ public class NameAnalyzer {
             IO.println("Invalid file name");
             return;
         }
+        int dotPosition = FileName.lastIndexOf('.');
+        String baseName = FileName.substring(0, dotPosition);
 
-        IO.println("Valid file name.");
+        String extension = FileName.substring(dotPosition + 1).toLowerCase();
+        IO.println("Base name: " + baseName);
+        IO.println("Extension: " + extension);
+
+        String category;
+
+        if (extension.equals("txt") || extension.equals("pdf") || extension.equals("docx")) {
+            category = "Document";
+        }
+        else if (extension.equals("jpg") || extension.equals("png") || extension.equals("gif")) {
+            category = "Image";
+        }
+        else if (extension.equals("mp3") || extension.equals("wav")) {
+            category = "Audio";
+        }
+        else {
+            category = "Other";
+        }
+
+        IO.println("Base name: " + baseName);
+        IO.println("Extension: " + extension);
+        IO.println("Category: " + category);
+
     }
 }

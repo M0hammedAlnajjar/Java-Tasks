b17b3e288b3b5022302060e0389b57a323ed1096
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-25 17:29:14 +0400

    Part 1: Create the class and store CSV records

--- Java Task Set 5/src/FileInventoryReport.java
+++ Java Task Set 5/src/FileInventoryReport.java
@@ -1,6 +1,16 @@
 public class FileInventoryReport {
-    static void main() {
+
+    public static void main(String[] args) {
+
+        // Store CSV file records
+        String[] files = {
+                "photo.png,340",
+                "report.pdf,120",
+                "music.mp3,500",
+                "Main.java,80",
+                "data.xlsx,200",
+                "unknown.xyz,50"
+        };
 
     }
-
 }

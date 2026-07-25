54640a472c3a7a4cd7d56e28bc4d794ef3b3a8d6
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-25 17:20:23 +0400

    // Split data using comma

--- Java Task Set 5/src/CsvLineParser.java
+++ Java Task Set 5/src/CsvLineParser.java
@@ -6,6 +6,9 @@ public class CsvLineParser {
         String Read=IO.readln(" Enter your name ,age and city in one line ");
         double d1  = Double.parseDouble(Read);
 
+        // Split data using comma
+        String[] data = read.split(",");
+
     }
 
 }

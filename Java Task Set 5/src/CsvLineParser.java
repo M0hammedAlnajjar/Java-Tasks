3d40c9f784e824aa29f640af53b84d12fff811be
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-25 17:21:47 +0400

    Display information

--- Java Task Set 5/src/CsvLineParser.java
+++ Java Task Set 5/src/CsvLineParser.java
@@ -3,8 +3,8 @@ public class CsvLineParser {
     static void main() {
 
 
-        String Read=IO.readln(" Enter your name ,age and city in one line ");
-        double d1  = Double.parseDouble(Read);
+        String Read = IO.readln(" Enter your name ,age and city in one line ");
+        double d1 = Double.parseDouble(Read);
 
         // Split data using comma
         String[] data = read.split(",");
@@ -14,6 +14,12 @@ public class CsvLineParser {
         int age = Integer.parseInt(data[1].trim());
         String city = data[2].trim();
 
-    }
 
+        // Display information
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+
+    }
 }
+

c4c94257ee19f5b1ce66b8ace984b59dc9d36f07
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-25 15:17:17 +0400

    I add read input

--- Java Task Set 5/src/PathSplitter.java
+++ Java Task Set 5/src/PathSplitter.java
@@ -1,2 +1,13 @@
 public class PathSplitter {
+
+    static void main() {
+
+        String NameFile = IO.readln("Enter the NameFile : ");
+        double base = Double.parseDouble(NameFile);
+
+    }
+
+
+
+
 }

7b22f1ba35b1b48e81edbfd849f0f30666abcc58
mohammed <mooodo150@gmail.com>
2026-07-26 12:03:37 +0400

    4. Create a HashSet to store student IDs.

--- Java Task 6/src/StudentSetManager.java
+++ Java Task 6/src/StudentSetManager.java
@@ -1,15 +1,20 @@
+import java.util.HashMap;
+
 public class StudentSetManager {
 
-    static void main() {
+    public static void main(String[] args) {
 
-        Integer   names ;
-        Double studentIDs;
+        HashMap<String, Integer> studentScores = new HashMap<>();
+
+        studentScores.put("Mohammed", 25);
+        studentScores.put("Ali", 55);
+        studentScores.put("Reship", 23);
+
+        IO.println(studentScores);
+
+        IO.println("Ali's score: " + studentScores.get("Ali"));
 
 
 
     }
-
-
-
 }
-

1c0d652d22c6f08a83d3ccdcd5a155eec08693ce
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-26 23:55:10 +0400

    5. Prompt the user to enter the number of products using a Scanner.

--- Java Task 6/src/ProductInventorySet.java
+++ Java Task 6/src/ProductInventorySet.java
@@ -2,10 +2,12 @@ import java.util.HashSet;
 
 public class ProductInventorySet {
 
+    public static void main(String[] args) {
 
-    public static void main(String args[]) {
-        HashSet<Integer> ProductName = new HashSet<>();
+        HashSet<Integer> productIDs = new HashSet<>();
 
+        String number = IO.readln("Enter number of products: ");
+        int totalProducts = Integer.parseInt(number);
 
     }
 }

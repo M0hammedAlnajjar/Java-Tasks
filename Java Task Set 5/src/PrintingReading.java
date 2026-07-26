d8ad19da3ba9d34077066e7fe68bd92368d2383e
mohammed <mooodo150@gmail.com>
2026-07-26 08:28:25 +0400

    I add input in modern moode

---
+++ Java Task Set 5/src/PrintingReading.java
@@ -0,0 +1,12 @@
+public class PrintingReading {
+
+    static void main() {
+        String Age =IO.readln("Enter your age :");
+        double A1=Double.parseDouble(Age);
+
+        IO.println("Your age is : "+ Age);
+
+
+
+    }
+}

1144856ffabd722ccc1ffcf1379de6d64b496127
mohammed <mooodo150@gmail.com>
2026-07-26 11:39:42 +0400

    Add Java Task 6 in tasks

--- Java Task Set 5/src/SetExample.java
+++ Java Task Set 5/src/SetExample.java
@@ -1,6 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
 public class SetExample {
 
     static void main() {
+                HashSet<String> f1 = new HashSet<>();
 
-    }
-}
+                f1.add("Ali");
+                f1.add("Mohammed");
+                f1.add("NOOR");
+                IO.println(f1);
+                IO.println(f1.size());
+                Boolean isPreaent=f1.contains("mohammed");
+                IO.println(isPreaent);
+            }
+        }
+
+
+

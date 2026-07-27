75c6b53956edcbf13a8ef2535f49071b885bdffd
mohammed <mooodo150@gmail.com>
2026-07-27 14:01:12 +0400

    Use the remove() method to delete the course from the HashSet.

--- Java Task 6/src/CourseEnrollmentSet.java
+++ Java Task 6/src/CourseEnrollmentSet.java
@@ -3,30 +3,35 @@ import java.util.HashSet;
 public class CourseEnrollmentSet {
     static void main() {
 
-        HashSet<Integer>  CourseNames = new HashSet<>();
+        HashSet<Integer> courseNames = new HashSet<>();
 
-        String courses =IO.readln("Enter the number of courses  : ");
-        Integer b1 =Integer.parseInt(courses);
+        String courses = IO.readln("Enter the number of courses: ");
+        int b1 = Integer.parseInt(courses);
 
-        if (b1<=0){
-            IO.println("Invalid number of courses ");
-        }
-        else {
-            for(int i= 1 ; i<= b1; i++) {
+        if (b1 <= 0) {
+            IO.println("Invalid number of courses");
+        } else {
 
-                String a = IO.readln("Enter course name  " + i + ": ");
+            for (int i = 1; i <= b1; i++) {
+                String a = IO.readln("Enter course ID " + i + ": ");
                 int id = Integer.parseInt(a);
 
-            if (CourseNames.add(id)){
-                IO.println("Course already exists");
-
+                if (courseNames.add(id)) {
+                    IO.println("Course added successfully.");
+                } else {
+                    IO.println("Duplicate entries are not allowed.");
+                }
             }
-            else {
-                IO.println(" Duplicate entries are not allowed");
-            }
 
+            // Remove a course
+            String removeCourse = IO.readln("Enter course ID to remove: ");
+            int removeId = Integer.parseInt(removeCourse);
+
+            if (courseNames.remove(removeId)) {
+                IO.println("Course removed successfully.");
+            } else {
+                IO.println("Course not found.");
             }
         }
-
     }
 }

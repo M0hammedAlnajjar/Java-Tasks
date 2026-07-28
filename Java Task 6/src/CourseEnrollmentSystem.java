00606561621f328e82311f992e2ac70d94f93481
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 17:53:39 +0400

    I Declare variables

--- Java Task 6/src/CourseEnrollmentSystem.java
+++ Java Task 6/src/CourseEnrollmentSystem.java
@@ -11,6 +11,15 @@ public class CourseEnrollmentSystem {
         // Create HashMap to store student IDs and course names
         HashMap<Integer, String> studentCourses = new HashMap<>();
 
+// Declare variables
+        String n = IO.readln("Enter number of students: ");
+        int numberOfStudents = Integer.parseInt(n);
+
+        int studentId;
+        String courseName;
+        int updateStudentId;
+        String newCourseName;
+        String enrollmentClassification;
     }
 
 }

cb9c4fe09214d45f218146b1d2e4adf6afd9171a
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 16:36:34 +0400

    I Create HashMap to store student IDs and grades

--- Java Task 6/src/StudentGradesMap.java
+++ Java Task 6/src/StudentGradesMap.java
@@ -4,5 +4,9 @@ public class StudentGradesMap {
 
     public static void main() {
 
+        // Create HashMap to store student IDs and grades
+        HashMap<Integer, Double> studentGrades = new HashMap<>();
+
+
     }
 }

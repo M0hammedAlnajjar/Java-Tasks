ae4bf730a2340aa03a0f0b09e58162a15fcb69e8
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 16:58:43 +0400

    I Create HashMap to store product names and prices

--- Java Task 6/src/ProductPriceCatalog.java
+++ Java Task 6/src/ProductPriceCatalog.java
@@ -3,6 +3,10 @@ public class ProductPriceCatalog {
 
     public static void main() {
 
+        // Create HashMap to store product names and prices
+        HashMap<String, Double> productCatalog = new HashMap<>();
+
+
     }
 
 }

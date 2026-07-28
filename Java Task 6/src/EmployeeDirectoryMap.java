7713a8348592eeba745596bd84e07abaf817e6a4
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 16:02:06 +0400

    I  Create a HashMap<Integer, String> to store employee IDs as keys and employee

--- Java Task 6/src/EmployeeDirectoryMap.java
+++ Java Task 6/src/EmployeeDirectoryMap.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
 public class EmployeeDirectoryMap {
 
     public static void main() {
+
+        // Create HashMap to store employee IDs and names
+        HashMap<Integer, String> employeeDirectory = new HashMap<>();
 
     }
 }

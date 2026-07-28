0ea3253a60e57afa424bb9f98da6bdb7e162bbe3
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 19:47:06 +0400

    I Create HashMap to store employee IDs and salaries

--- Java Task 6/src/EmployeeSalaryManager.java
+++ Java Task 6/src/EmployeeSalaryManager.java
@@ -4,6 +4,10 @@ public class EmployeeSalaryManager {
 
 
     public static void main() {
+        // Create HashMap to store employee IDs and salaries
+        HashMap<Integer, Double> employeeSalaries = new HashMap<>();
+
+
 
     }
 }

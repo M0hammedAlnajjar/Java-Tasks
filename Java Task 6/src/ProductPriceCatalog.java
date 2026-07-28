016d040a120f8baf8a19c22fb3e6eae161ef87b5
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 16:59:18 +0400

    I Declare variables

--- Java Task 6/src/ProductPriceCatalog.java
+++ Java Task 6/src/ProductPriceCatalog.java
@@ -6,6 +6,17 @@ public class ProductPriceCatalog {
         // Create HashMap to store product names and prices
         HashMap<String, Double> productCatalog = new HashMap<>();
 
+        // Declare variables
+        String n = IO.readln("Enter number of products: ");
+        int numberOfProducts = Integer.parseInt(n);
+
+        String productName;
+        double productPrice;
+        String searchProduct;
+        String updateChoice;
+        double newPrice;
+        String searchResult;
+        String inventoryClassification;
 
     }
 

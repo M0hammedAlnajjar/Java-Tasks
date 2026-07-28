bed930d9dc8225c78c0c658f8804a8fc8863602a
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 19:58:59 +0400

    I  Declare variables

--- Java Task 6/src/StudentAttendanceManager.java
+++ Java Task 6/src/StudentAttendanceManager.java
@@ -8,5 +8,13 @@ public class StudentAttendanceManager {
         // Create HashMap to store student IDs and attendance days
         HashMap<Integer, Integer> attendanceRecords = new HashMap<>();
 
+        // Declare variables
+        String n = IO.readln("Enter number of student records: ");
+        int numberOfStudents = Integer.parseInt(n);
+
+        int studentId;
+        int attendanceDays;
+        int choice;
+
     }
 }

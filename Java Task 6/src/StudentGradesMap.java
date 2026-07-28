bebf355a1dbf4ad4ae8363ebb19dc47f60bec22c
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 16:36:58 +0400

    I Declare variables

--- Java Task 6/src/StudentGradesMap.java
+++ Java Task 6/src/StudentGradesMap.java
@@ -6,7 +6,17 @@ public class StudentGradesMap {
 
         // Create HashMap to store student IDs and grades
         HashMap<Integer, Double> studentGrades = new HashMap<>();
+// Declare variables
+        String n = IO.readln("Enter number of students: ");
+        int numberOfStudents = Integer.parseInt(n);
 
+        int studentId;
+        double grade;
+        int updateStudentId;
+        double newGrade;
+        double totalGrades = 0;
+        double averageGrade;
+        String classPerformance;
 
     }
 }

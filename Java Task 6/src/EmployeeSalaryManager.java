87ada31de162d3949050e7967b6b8f09af4b4123
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 19:47:26 +0400

    Declare variables

--- Java Task 6/src/EmployeeSalaryManager.java
+++ Java Task 6/src/EmployeeSalaryManager.java
@@ -6,7 +6,14 @@ public class EmployeeSalaryManager {
     public static void main() {
         // Create HashMap to store employee IDs and salaries
         HashMap<Integer, Double> employeeSalaries = new HashMap<>();
+// Declare variables
+        String n = IO.readln("Enter number of employees: ");
+        int numberOfEmployees = Integer.parseInt(n);
 
+        int employeeId;
+        double salary;
+
+        int choice;
 
 
     }

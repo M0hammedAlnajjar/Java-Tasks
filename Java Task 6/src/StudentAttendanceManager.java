fd90c0070b1d3e75d6b0ec56afece802f3d332c6
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 19:57:42 +0400

    I Create HashMap to store student IDs and attendance days

--- Java Task 6/src/StudentAttendanceManager.java
+++ Java Task 6/src/StudentAttendanceManager.java
@@ -3,6 +3,8 @@ public class StudentAttendanceManager {
 
     public static void main() {
 
+        // Create HashMap to store student IDs and attendance days
+        HashMap<Integer, Integer> attendanceRecords = new HashMap<>();
 
     }
 }

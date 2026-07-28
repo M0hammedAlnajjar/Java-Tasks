3c76a5c39502dc44db46126c4cc27c569277b759
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 15:52:42 +0400

    Create a TreeSet<String> to store city names.

--- Java Task 6/src/CityDirectorySet.java
+++ Java Task 6/src/CityDirectorySet.java
@@ -3,6 +3,9 @@ import java.util.TreeSet;
 public class CityDirectorySet {
 
     public static void main() {
+        // Create TreeSet to store unique city names
+        TreeSet<String> cityDirectory = new TreeSet<>();
+
 
     }
 }

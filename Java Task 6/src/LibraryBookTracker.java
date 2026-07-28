78e44c0acda848fa4813678fd420782c4c8fd40f
Mohammed Alnajjar <mooodo150@gmail.com>
2026-07-28 17:29:15 +0400

    I Create HashMap to store book IDs and titles

--- Java Task 6/src/LibraryBookTracker.java
+++ Java Task 6/src/LibraryBookTracker.java
@@ -7,5 +7,11 @@ public class LibraryBookTracker {
     public static void main() {
         // Create HashSet to store unique book IDs
         HashSet<Integer> bookIds = new HashSet<>();
+
+        // Create HashMap to store book IDs and titles
+        HashMap<Integer, String> bookTitles = new HashMap<>();
+
+
+
     }
 }

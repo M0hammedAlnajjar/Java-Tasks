f03813308aebeaee4c3cbddf2b258e9ed1b53d4b
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-01 20:31:18 +0400

    I create Prompt the user to enter the number of students using a Scanner.

--- Java Task 7/src/StudentNameManager.java
+++ Java Task 7/src/StudentNameManager.java
@@ -4,5 +4,7 @@ public class StudentNameManager {
 
         String [] student ={"mohammed ", "Ali", "noor"};
 
+        String Student=IO.readln("Enter the numbers of students ");
+        int d1=Integer.parseInt(Student);
     }
 }

4854b9bdffba7dd570ca86679d446ec4a7f16e1b
mohammed <mooodo150@gmail.com>
2026-08-02 16:16:03 +0400

    I use for loop inside loop

--- Java Task 7/src/UndoActionManager.java
+++ Java Task 7/src/UndoActionManager.java
@@ -4,15 +4,18 @@ public class UndoActionManager {
 
     static void main() {
 
-        String actions=IO.readln("Enter  the number of actions :");
-        Integer number=Integer.parseInt(actions);
+        String actions = IO.readln("Enter the number of actions:");
+        int number = Integer.parseInt(actions);
 
-
-        if (number<=0){
+        if (number <= 0) {
             IO.println("Invalid number of actions.");
+        } else {
+            Stack<String> stack = new Stack<>();
 
+            for (int i = 0; i < number; i++) {
+                String action = IO.readln("Enter action:");
+                stack.push(action);
+            }
         }
-
-
     }
 }

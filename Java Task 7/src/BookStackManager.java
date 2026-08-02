de8a3d5be6f0ce8af7141539f2e2b050ccc598b0
mohammed <mooodo150@gmail.com>
2026-08-02 11:40:56 +0400

    I Use a for loop to read each book title

--- Java Task 7/src/BookStackManager.java
+++ Java Task 7/src/BookStackManager.java
@@ -3,10 +3,20 @@ import java.util.Stack;
 public class BookStackManager {
 
 
-    static void main() {
+    public static void main(String[] args) {
 
-        String Books =IO.readln("Enter the number of books  ");
-        int BookStack =Integer.parseInt(Books);
+        String Books = IO.readln("Enter the number of books : ");
+        int BookStack = Integer.parseInt(Books);
+
+        if (BookStack <= 0) {
+            IO.println("Invalid number of books");
+        }
+
+        else {
+            for (int i =0;i<BookStack ; i++){
+
+            }
+        }
 
 
     }

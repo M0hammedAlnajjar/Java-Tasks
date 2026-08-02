ff44e54a6d4261cd1f1aa8b74568b5b2ce641507
mohammed <mooodo150@gmail.com>
2026-08-02 11:30:16 +0400

    I Prompt the user to enter the number of books

--- Java Task 7/src/BookStackManager.java
+++ Java Task 7/src/BookStackManager.java
@@ -5,5 +5,9 @@ public class BookStackManager {
 
     static void main() {
 
+        String Books =IO.readln("Enter the number of books  ");
+        int BookStack =Integer.parseInt(Books);
+
+
     }
 }

52c3d15895c1cc07f08c844fe56651e10fe20b42
mohammed <mooodo150@gmail.com>
2026-08-02 08:29:15 +0400

    Prompt the user to enter a password using

--- Java Task 7/src/PasswordStrengthChecker.java
+++ Java Task 7/src/PasswordStrengthChecker.java
@@ -2,5 +2,8 @@ public class PasswordStrengthChecker {
 
     static void main() {
 
+        String  password=IO.readln("Enter a password using a Scanner");
+        Integer UserPassword=Integer.parseInt(password);
+
     }
 }

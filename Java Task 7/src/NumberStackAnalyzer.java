873e0d49a0c7af13252ab719c2b723a57d48dd4d
mohammed <mooodo150@gmail.com>
2026-08-02 16:41:46 +0400

    I Prompt the user to enter the number of values

--- Java Task 7/src/NumberStackAnalyzer.java
+++ Java Task 7/src/NumberStackAnalyzer.java
@@ -3,6 +3,10 @@ import java.util.Stack;
 public class NumberStackAnalyzer {
     static void main(String[] args) {
 
+        String values=IO.readln("Enter the number of values : ");
+        Integer  number=Integer.parseInt(values);
+
+
 
 
 

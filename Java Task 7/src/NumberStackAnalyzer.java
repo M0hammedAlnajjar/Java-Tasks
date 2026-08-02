9f0720318cfa586b5f3a92f0a31dc0f2f9b9eedb
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-02 20:17:21 +0400

    feat: add stack menu using do-while and switch-case

--- Java Task 7/src/NumberStackAnalyzer.java
+++ Java Task 7/src/NumberStackAnalyzer.java
@@ -24,6 +24,54 @@ public class NumberStackAnalyzer {
 
             }
 
+            int choice;
+
+            do {
+
+                System.out.println("\n===== Number Stack Analyzer =====");
+                System.out.println("1. Add Number");
+                System.out.println("2. Remove Top Number");
+                System.out.println("3. View Top Number");
+                System.out.println("4. Display All Numbers");
+                System.out.println("5. Analyze Numbers");
+                System.out.println("6. Exit");
+
+                System.out.print("Enter your choice: ");
+                choice = scanner.nextInt();
+
+                switch (choice) {
+
+                    case 1:
+                        System.out.println("Add Number");
+                        break;
+
+                    case 2:
+                        System.out.println("Remove Top Number");
+                        break;
+
+                    case 3:
+                        System.out.println("View Top Number");
+                        break;
+
+                    case 4:
+                        System.out.println("Display All Numbers");
+                        break;
+
+                    case 5:
+                        System.out.println("Analyze Numbers");
+                        break;
+
+                    case 6:
+                        System.out.println("Exiting program...");
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice.");
+
+                }
+
+            } while (choice != 6);
+
         }
 
     }

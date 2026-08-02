a99426a59bd385c4951c3f4f08983fbb7027c8e3
mohammed <mooodo150@gmail.com>
2026-08-02 11:53:28 +0400

    I Add each book to the stack using the push() method

--- Java Task 7/src/BookStackManager.java
+++ Java Task 7/src/BookStackManager.java
@@ -8,12 +8,17 @@ public class BookStackManager {
         String Books = IO.readln("Enter the number of books : ");
         int BookStack = Integer.parseInt(Books);
 
+        Stack<String> books = new Stack<>();
+
         if (BookStack <= 0) {
             IO.println("Invalid number of books");
         }
 
         else {
             for (int i =0;i<BookStack ; i++){
+
+                String BookTitle = IO.readln("Enter book title : ");
+                books.push(BookTitle);
 
             }
         }

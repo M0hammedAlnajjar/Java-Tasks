d85c7b4dcab10bb1485006378ebe9ec51e28eb9d
mohammed <mooodo150@gmail.com>
2026-08-03 11:17:20 +0400

    I create CompletedRegistrations stack

--- Java Task 7/src/StudentRegistrationManager.java
+++ Java Task 7/src/StudentRegistrationManager.java
@@ -1,11 +1,14 @@
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class StudentRegistrationManager {
 
     static void main(String[] args) {
 
         Queue<String> StudentsWaiting = new LinkedList<>();
+
+        Stack<String> CompletedRegistrations  = new Stack<>();
 
 
 

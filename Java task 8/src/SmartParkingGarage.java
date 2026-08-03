16551a73dc43078033ea5221f610969538de9a78
mohammed <mooodo150@gmail.com>
2026-08-03 15:08:10 +0400

    Part 1 – Vehicle Registration

--- Java task 8/src/SmartParkingGarage.java
+++ Java task 8/src/SmartParkingGarage.java
@@ -6,39 +6,51 @@ import java.util.LinkedList;
 public class SmartParkingGarage {
     public static void main(String[] args) {
 
+        // Queue to store waiting vehicles
         Queue<String> waitingVehicles = new LinkedList<>();
 
+        // Stack to store parked vehicles
         Stack<String> parkedVehicles = new Stack<>();
 
+        // HashSet to store unique license plates
         HashSet<String> licensePlates = new HashSet<>();
 
+        // Maximum parking capacity
         int maximum = 10;
 
-        String license = IO.readln("Enter plate number: ");
+        while (true) {
 
-        if (licensePlates.contains(license)) {
-            System.out.println("Vehicle already exists. License plate must be unique.");
-        }
-        else {
-            // Add new license plate
-            licensePlates.add(license);
+            String license = IO.readln("Enter plate number (or type EXIT to stop): ");
 
-            // Check parking capacity
-            if (parkedVehicles.size() < maximum) {
-                parkedVehicles.push(license);
-                System.out.println("Vehicle parked successfully.");
+            if (license.equalsIgnoreCase("EXIT")) {
+                break;
             }
 
+            // Check if vehicle already exists
+            if (licensePlates.contains(license)) {
+                System.out.println("Error: Vehicle already exists. License plate must be unique.");
+            }
             else {
-                waitingVehicles.add(license);
-                System.out.println("Parking full. Vehicle added to waiting queue.");
+                // Add unique license plate
+                licensePlates.add(license);
+
+                // Check parking capacity
+                if (parkedVehicles.size() < maximum) {
+                    parkedVehicles.push(license);
+                    System.out.println("Vehicle parked successfully.");
+                }
+                else {
+                    waitingVehicles.add(license);
+                    System.out.println("Parking full. Vehicle added to waiting queue.");
+                }
             }
+
+            System.out.println("Parked vehicles: " + parkedVehicles);
+            System.out.println("Waiting vehicles: " + waitingVehicles);
+            System.out.println();
         }
 
-        System.out.println("Parked vehicles: " + parkedVehicles);
-        System.out.println("Waiting vehicles: " + waitingVehicles);
+        System.out.println("Final parked vehicles: " + parkedVehicles);
+        System.out.println("Final waiting vehicles: " + waitingVehicles);
     }
-
-
-        }
 }

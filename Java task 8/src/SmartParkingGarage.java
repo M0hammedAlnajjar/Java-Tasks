8c94cc190a5c0f703cc9ca899b792d3446a23405
mohammed <mooodo150@gmail.com>
2026-08-03 14:52:39 +0400

    Add new license plate

--- Java task 8/src/SmartParkingGarage.java
+++ Java task 8/src/SmartParkingGarage.java
@@ -2,27 +2,23 @@ import java.util.HashSet;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.LinkedList;
-import java.util.Set;
-
 
 public class SmartParkingGarage {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         Queue<String> waitingVehicles = new LinkedList<>();
 
         Stack<String> parkedVehicles = new Stack<>();
 
-        HashSet<String> plateNumber=new  HashSet<>();
+        HashSet<String> licensePlates = new HashSet<>();
 
-        int  maximum=10;
+        int maximum = 10;
 
-        String  license =IO.readln("Enter plate number");
-        if (licensePlates.contains(license)){
-            IO.println(" System.out.println(\"Vehicle already exists. License plate must be unique.\");");
+        String license = IO.readln("Enter plate number: ");
+
+        if (licensePlates.contains(license)) {
+            System.out.println("Vehicle already exists. License plate must be unique.");
         }
-
-
-
 
     }
 }

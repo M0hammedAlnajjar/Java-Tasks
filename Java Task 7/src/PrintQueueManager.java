78d1e0b0af806a30563694f30d067d725d93af83
mohammed <mooodo150@gmail.com>
2026-08-03 10:32:09 +0400

    I create Handle invalid menu choices

--- Java Task 7/src/PrintQueueManager.java
+++ Java Task 7/src/PrintQueueManager.java
@@ -108,6 +108,9 @@ public class PrintQueueManager {
                         break;
 
 
+                    // Handle invalid menu choices
+                    default:
+                        IO.println("Invalid option. Please try again.");
 
 
 

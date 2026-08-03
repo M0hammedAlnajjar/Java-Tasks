7af4ba00240ea77a002ed6fffa5795fb559cd0f6
mohammed <mooodo150@gmail.com>
2026-08-03 12:20:35 +0400

    I use for loop for add and read

--- Java Task 7/src/HospitalPatientManager.java
+++ Java Task 7/src/HospitalPatientManager.java
@@ -1,17 +1,29 @@
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Stack;
+
 public class HospitalPatientManager {
 
     static void main(String[] args) {
 
         Queue<String> PatientNames = new LinkedList<>();
 
-        Stack<String> CompletedPatient= new Stack<>();
+        Stack<String> CompletedPatient = new Stack<>();
 
+        String patients = IO.readln("Enter the number of patients waiting: ");
+        int number = Integer.parseInt(patients);
 
-        String patients =IO.readln("Enter  the number of patients waiting ");
-        int number =Integer.parseInt(patients);
+        if (number <= 0) {
+            IO.println("Invalid number of patients");
 
+        } else {
+
+            for (int i = 0; i < number; i++) {
+
+                String patient = IO.readln("Enter the patient name: ");
+
+                PatientNames.add(patient);
+            }
+        }
     }
 }

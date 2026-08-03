05fecb03985dbfb821aa0d8776b232a916231ac1
mohammed <mooodo150@gmail.com>
2026-08-03 08:54:02 +0400

    I create prompt the user to enter the number of customers

--- Java Task 7/src/CustomerQueueManager.java
+++ Java Task 7/src/CustomerQueueManager.java
@@ -2,5 +2,9 @@ public class CustomerQueueManager {
 
     static void main(String[] args) {
 
+        String customers =IO.readln("Enter the number of customers");
+        int number =Integer.parseInt(customers);
+
+
     }
 }

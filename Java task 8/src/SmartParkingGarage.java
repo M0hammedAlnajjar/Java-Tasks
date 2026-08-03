1f5c7229f134470fe21e17017a7ecb809099f589
mohammed <mooodo150@gmail.com>
2026-08-03 14:18:35 +0400

    I  Queue<String> waitingVehicles = new LinkedList<>();

--- Java task 8/src/SmartParkingGarage.java
+++ Java task 8/src/SmartParkingGarage.java
@@ -4,6 +4,8 @@ import java.util.Stack;
 public class SmartParkingGarage {
     static void main(String[] args) {
 
+        Queue<String> waitingVehicles = new LinkedList<>();
+
         Stack<String> parkedVehicles = new Stack<>();
 
     }

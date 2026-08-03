5aa157b70e03c79a47f71bf1c12f8ae8e744c687
mohammed <mooodo150@gmail.com>
2026-08-03 09:47:14 +0400

    I Create for loop for add

--- Java Task 7/src/PrintQueueManager.java
+++ Java Task 7/src/PrintQueueManager.java
@@ -5,16 +5,22 @@ public class PrintQueueManager {
 
     static void main(String[] args) {
 
-        Queue<String>  jobname = new LinkedList<>();
+        Queue<String> jobname = new LinkedList<>();
 
-        String  jobs = IO.readln("Enter the number of print jobs : ");
-        int number =Integer.parseInt(jobs);
+        String jobs = IO.readln("Enter the number of print jobs: ");
+        int number = Integer.parseInt(jobs);
 
+        if (number <= 0) {
 
-        if (number<=0){
-            IO.println("Invalid number of print jobs ");
+            IO.println("Invalid number of print jobs");
 
+        } else {
+
+            for (int i = 0; i < number; i++) {
+
+                String jobName = IO.readln("Enter print job name: ");
+                jobname.offer(jobName);
+            }
         }
-
     }
 }

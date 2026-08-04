2018bc175d647dfb10c836600f7f5004018df8e0
mohammed <mooodo150@gmail.com>
2026-08-04 16:39:56 +0400

    Create the base case

--- Java Task 9/src/LengthString.java
+++ Java Task 9/src/LengthString.java
@@ -1,9 +1,12 @@
 public class LengthString {
     static int Length(int n) {
+        // Base case
+        if (n < 10) {
+            return 1;
 
-
+        }
+        return n;
     }
-
     static void main() {
 
     }

6b4c9fa7535f4316ab938c92fbf06d3d237c85fb
mohammed <mooodo150@gmail.com>
2026-08-04 14:28:51 +0400

    I add Base case

--- Java Task 9/src/PrintNumbers.java
+++ Java Task 9/src/PrintNumbers.java
@@ -1,8 +1,14 @@
 public class PrintNumbers {
     static void printNumbers (int n){
-
+            // Base case
+            if (n == 0) {
+                return;
 
     }
+
+    }
+
+
     static void main() {
 
     }

02d6b20dc89b9f0a2106e4f546fed0e7d0c4f044
mohammed <mooodo150@gmail.com>
2026-08-04 12:19:45 +0400

    Task 375 — Count Down and Count Up it is done 100 %

--- Java Task 9/src/CountDownCountUp.java
+++ Java Task 9/src/CountDownCountUp.java
@@ -7,15 +7,18 @@ public class CountDownCountUp {
         }
         IO.println(n);
 
-        count(n - 1);
-        IO.println(n);
+        if (n>1) {
+            count(n - 1);
+            IO.println(n);
 
-
+        }
     }
         public static void main(String[] args) {
 
-            String countdown=IO.readln("Enter your number ");
+            String countdown=IO.readln("Enter your number : ");
             int number=Integer.parseInt(countdown);
+
+            count(number);
 
         }
     }

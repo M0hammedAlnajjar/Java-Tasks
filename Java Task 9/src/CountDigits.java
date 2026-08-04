2a568ec3b7127e5e0500752159059138ae60f298
mohammed <mooodo150@gmail.com>
2026-08-04 15:15:46 +0400

    I add base case

--- Java Task 9/src/CountDigits.java
+++ Java Task 9/src/CountDigits.java
@@ -1,7 +1,15 @@
 public class CountDigits {
+    static int countDigits(int n) {
+        // Base case
+        if (n < 10) {
+            return 1;
 
+    }
+        return n;
+    }
 
     static void main(String[] args) {
 
     }
+
 }

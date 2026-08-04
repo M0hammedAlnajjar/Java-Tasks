116bdf8a0743998a38e3f04468cc911a78642d65
mohammed <mooodo150@gmail.com>
2026-08-04 12:05:42 +0400

    Print before recursion

--- Java Task 9/src/CountDownCountUp.java
+++ Java Task 9/src/CountDownCountUp.java
@@ -1,9 +1,19 @@
 public class CountDownCountUp {
 
-    static void count(int n)  {
-        if (n==1){
+    static void count(int n) {
+        if (n == 1) {
             return;
         }
+        IO.println(n);
+
+
+        count(n - 1);
+        IO.println(n);
+
 
     }
-}
+        public static void main(String[] args) {
+            count(3);
+        }
+    }
+

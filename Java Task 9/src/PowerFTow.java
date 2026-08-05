9887da9df0138436adc1fdfc3129885881e79834
mohammed <mooodo150@gmail.com>
2026-08-05 10:30:36 +0400

    Task 381 — Power of 2 Check  it is done 100 %

--- Java Task 9/src/PowerFTow.java
+++ Java Task 9/src/PowerFTow.java
@@ -1,18 +1,25 @@
-public class PowerFTow {
-
-
+public class  PowerFTow {
 
     static boolean isPowerOfTwo(int n) {
-            // Base cases
-            if (n == 1) {
-                return true;
-            }
+        // Base cases
+        if (n == 1) {
+            return true;
+        }
 
         if (n == 0 || n % 2 != 0) {
             return false;
-
-            // Recursive case
-            return isPowerOfTwo(n / 2);
         }
 
+        // Recursive case
+        return isPowerOfTwo(n / 2);
+    }
+
+    public static void main(String[] args) {
+
+        IO.println("=== Power of 2 Check ===");
+
+        int number = Integer.parseInt(IO.readln("Enter a number: "));
+
+        IO.println("Is power of 2 ? " + isPowerOfTwo(number));
+    }
 }

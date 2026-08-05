f0577c3c9455fcad6f304b65fd366e1bca8da047
mohammed <mooodo150@gmail.com>
2026-08-05 10:56:17 +0400

    Create if for bace case

--- Java Task 9/src/CharacterCount.java
+++ Java Task 9/src/CharacterCount.java
@@ -1,6 +1,9 @@
 public class CharacterCount {
     static int countChar(String text, char letter){
-
+        // Base case
+        if (text.length() == 0) {
+            return 0;
+        }
     }
 
     static void main(String[] args) {

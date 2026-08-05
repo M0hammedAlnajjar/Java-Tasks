47cf74ae2b55f3f27b62238984ed42205f704261
mohammed <mooodo150@gmail.com>
2026-08-05 11:06:44 +0400

    Create the base case and recursive case

--- Java Task 9/src/SumArray.java
+++ Java Task 9/src/SumArray.java
@@ -1,8 +1,15 @@
 public class SumArray {
 
     static int sumArray(int[] numbers, int index){
+        // Base case
+        if (index == numbers.length) {
+            return 0;
+        }
 
+        // Recursive case
+        return numbers[index] + sumArray(numbers, index + 1);
     }
+
     static void main(String[] args) {
 
     }

4ca14d39747b3621c887e890670f132bf14acd43
mohammed <mooodo150@gmail.com>
2026-08-05 10:45:15 +0400

    Create the If first and last characters are different AND Base case

--- Java Task 9/src/WordPalindrome.java
+++ Java Task 9/src/WordPalindrome.java
@@ -1,6 +1,14 @@
 public class WordPalindrome {
 
     static boolean isPalindrome(String text){
+        // Base case
+        if (text.length() <= 1) {
+            return true;
+        }
+        // If first and last characters are different
+        if (text.charAt(0) != text.charAt(text.length() - 1)) {
+            return false;
+        }
 
     }
     static void main(String[] args) {

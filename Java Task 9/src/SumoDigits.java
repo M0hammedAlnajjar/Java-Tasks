f6d08a2b317254a4ee33ee7257d37e3ff1d72751
mohammed <mooodo150@gmail.com>
2026-08-05 08:25:47 +0400

    Create diplay

--- Java Task 9/src/SumoDigits.java
+++ Java Task 9/src/SumoDigits.java
@@ -1,16 +1,19 @@
 public class SumoDigits {
-    static int sumDigits(String n) {
+    static int sumDigits(Integer n) {
         // Base case
-        if (n== 0) {
+        if (n == 0) {
             return 0;
         }
+
         // Recursive case
         return (n % 10) + sumDigits(n / 10);
     }
 
-
     static void main() {
-
+        IO.println(5);;
+        IO.println(123);;
+        IO.println(sumDigits(999));
+        IO.println(4021);;
     }
 
 

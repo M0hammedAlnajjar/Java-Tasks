5d4587d735d28d02cc2217ad780f2400310b5019
mohammed <mooodo150@gmail.com>
2026-08-05 09:16:23 +0400

    Create Base case

--- Java Task 9/src/DisplayNumbers.java
+++ Java Task 9/src/DisplayNumbers.java
@@ -1,6 +1,9 @@
 public class DisplayNumbers {
     static void printUp(int n){
-
+        // Base case
+        if (n == 0) {
+            return;
+        }
     }
     static void main(String[] args) {
 

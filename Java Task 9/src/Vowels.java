fcf6338be5af772aea6f8541e5599018c215aa7f
mohammed <mooodo150@gmail.com>
2026-08-05 11:56:11 +0400

    If you want the program to not accept numbers and empty input, add checks before calling countVowels

--- Java Task 9/src/Vowels.java
+++ Java Task 9/src/Vowels.java
@@ -2,12 +2,10 @@ public class Vowels {
 
     static int countVowels(String text) {
         // Base case
-        if (text == null || text.length() == 0) {
+        if (text.length() == 0) {
             return 0;
         }
 
-
-        // Check first character
         char ch = Character.toLowerCase(text.charAt(0));
 
         int count = 0;
@@ -15,24 +13,28 @@ public class Vowels {
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             count = 1;
         }
-        // Recursive case
+
         return count + countVowels(text.substring(1));
     }
 
-
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         IO.println("=== Number of Vowels in a String ===");
 
         String text = IO.readln("Enter a word: ");
 
-
+        // Check empty input
         if (text == null || text.isEmpty()) {
             IO.println("Please enter a word!");
             return;
         }
 
+        // Check if input contains numbers
+        if (text.matches(".*\\d.*")) {
+            IO.println("Numbers are not allowed!");
+            return;
+        }
+
         IO.println("Number of vowels = " + countVowels(text));
     }
-
 }

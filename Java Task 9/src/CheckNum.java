c8ff934283fb7441279dd51af842ba9edaccf995
mohammed <mooodo150@gmail.com>
2026-08-05 10:35:09 +0400

    Create the Base cases

--- Java Task 9/src/CheckNum.java
+++ Java Task 9/src/CheckNum.java
@@ -1,6 +1,9 @@
 public class CheckNum {
     static boolean isEven(int n) {
-
+        // Base cases
+        if (n == 0) {
+            return true;
+        }
     }
 
     static void main(String[] args) {

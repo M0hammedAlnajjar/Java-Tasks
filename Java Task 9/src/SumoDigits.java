3f3fc9c0caa51eeb4751ffb823a403d4e587c790
mohammed <mooodo150@gmail.com>
2026-08-05 08:22:59 +0400

    Create the Recursive case and Base case

--- Java Task 9/src/SumoDigits.java
+++ Java Task 9/src/SumoDigits.java
@@ -1,8 +1,14 @@
 public class SumoDigits {
-    static int sumDigits(String n){
-
-
+    static int sumDigits(String n) {
+        // Base case
+        if (n== 0) {
+            return 0;
+        }
+        // Recursive case
+        return (n % 10) + sumDigits(n / 10);
     }
+
+
     static void main() {
 
     }

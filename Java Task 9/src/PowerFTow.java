eb4cd5810e0c48a6254a7f223e14c24ec4142af0
mohammed <mooodo150@gmail.com>
2026-08-05 09:38:26 +0400

    I create Recursive case

--- Java Task 9/src/PowerFTow.java
+++ Java Task 9/src/PowerFTow.java
@@ -7,5 +7,12 @@ public class PowerFTow {
             if (n == 1) {
                 return true;
             }
-    }
+
+        if (n == 0 || n % 2 != 0) {
+            return false;
+
+            // Recursive case
+            return isPowerOfTwo(n / 2);
+        }
+
 }

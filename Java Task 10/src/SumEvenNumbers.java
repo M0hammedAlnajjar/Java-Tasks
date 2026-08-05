626f4e9f2684934698d72f04c6aeb794296ef4d5
mohammed <mooodo150@gmail.com>
2026-08-05 16:23:54 +0400

    I Base case: stop when n reaches 0

--- Java Task 10/src/SumEvenNumbers.java
+++ Java Task 10/src/SumEvenNumbers.java
@@ -1,7 +1,10 @@
 public class SumEvenNumbers {
 
     public static int sumEven(int n) {
-
+        // Base case: stop when n reaches 0
+        if (n == 0) {
+            return 0;
+        }
 
     }
     static void main(String[] args) {

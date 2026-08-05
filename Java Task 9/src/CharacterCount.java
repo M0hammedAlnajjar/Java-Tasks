502d31e734dd46101132ec2a527ffc4d7437498f
mohammed <mooodo150@gmail.com>
2026-08-05 11:01:38 +0400

    I create when the user enters an empty string or the input is null, add a check before calling the recursive method.

--- Java Task 9/src/CharacterCount.java
+++ Java Task 9/src/CharacterCount.java
@@ -1,30 +1,40 @@
 public class CharacterCount {
-    static int countChar(String text, char letter){
+
+    static int countChar(String text, char letter) {
         // Base case
-        if (text.length() == 0) {
+        if (text == null || text.length() == 0) {
             return 0;
         }
 
-        // Check first character and add result of the rest
         int count = 0;
 
         if (text.charAt(0) == letter) {
             count = 1;
         }
-        // Recursive case
+
         return count + countChar(text.substring(1), letter);
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         IO.println("=== Count a Character in a String ===");
 
         String text = IO.readln("Enter a word: ");
+
+        if (text == null || text.isEmpty()) {
+            IO.println("You must enter a word!");
+            return;
+        }
+
         String inputLetter = IO.readln("Enter a letter: ");
+
+        if (inputLetter == null || inputLetter.isEmpty()) {
+            IO.println("You must enter a letter!");
+            return;
+        }
 
         char letter = inputLetter.charAt(0);
 
         IO.println("Count = " + countChar(text, letter));
-
     }
 }

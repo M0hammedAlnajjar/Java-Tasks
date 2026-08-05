f1ab0f78f2885b6d7d8ed32e8669b17901001fed
mohammed <mooodo150@gmail.com>
2026-08-05 11:40:16 +0400

    I do If the user enters characters or text instead of numbers, this line will cause an error

--- Java Task 9/src/MaximumArray.java
+++ Java Task 9/src/MaximumArray.java
@@ -1,33 +1,43 @@
-public class MaximumArray {
+public class MaximumArray{
 
-    static int findMax(int[] numbers, int index){
+    static int findMax(int[] numbers, int index) {
         // Base case: last element is the maximum for now
         if (index == numbers.length - 1) {
             return numbers[index];
         }
 
-        // Base case: last element is the maximum for now
+        // Recursive case
         int maxRest = findMax(numbers, index + 1);
 
         return Math.max(numbers[index], maxRest);
     }
-    static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         IO.println("=== Find Maximum in an Array ===");
+
         String input = IO.readln("Enter numbers separated by spaces: ");
 
         if (input == null || input.isEmpty()) {
             IO.println("Array is empty!");
             return;
         }
+
         String[] parts = input.split(" ");
 
         int[] numbers = new int[parts.length];
 
-        for (int i = 0; i < parts.length; i++) {
-            numbers[i] = Integer.parseInt(parts[i]);
+        // Convert String values to integers
+        try {
+            for (int i = 0; i < parts.length; i++) {
+                numbers[i] = Integer.parseInt(parts[i]);
+            }
+        } catch (NumberFormatException e) {
+            IO.println("Please enter numbers only!");
+            return;
         }
+
         IO.println("Maximum = " + findMax(numbers, 0));
     }
 }
+

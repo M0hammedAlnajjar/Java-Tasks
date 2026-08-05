35501e35dee569ce4fc105a54858a94476574b5a
mohammed <mooodo150@gmail.com>
2026-08-05 11:31:41 +0400

    Create Base case: last element is the maximum for now AND Base case: last element is the maximum for now

--- Java Task 9/src/MaximumArray.java
+++ Java Task 9/src/MaximumArray.java
@@ -1,6 +1,16 @@
 public class MaximumArray {
 
+    static int findMax(int[] numbers, int index){
+        // Base case: last element is the maximum for now
+        if (index == numbers.length - 1) {
+            return numbers[index];
+        }
 
+        // Recursive case: find max of the rest of the array
+        int maxRest = findMax(numbers, index + 1);
+
+        return Math.max(numbers[index], maxRest);
+    }
     static void main(String[] args) {
 
     }

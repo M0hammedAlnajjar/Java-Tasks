f2f60d84e4c0f90ffbd5fe0885b909c91b82fd36
mohammed <mooodo150@gmail.com>
2026-08-06 11:29:14 +0400

    I create  base case and If current element is bigger than next element

--- Java Task 10/src/CheckSortedArray.java
+++ Java Task 10/src/CheckSortedArray.java
@@ -1,7 +1,15 @@
 public class CheckSortedArray {
 
     public static boolean isSorted(int[] arr, int index) {
+        // Base case
+        if (index == arr.length - 1) {
+            return true;
+        }
 
+        // If current element is bigger than next element
+        if (arr[index] > arr[index + 1]) {
+            return false;
+        }
 
     }
 

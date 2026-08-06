d46087e0cd1a23191be89dc186d1072417bd6370
mohammed <mooodo150@gmail.com>
2026-08-06 09:40:30 +0400

    I modify task 390

--- Java Task 10/src/CountDigitRecursive.java
+++ Java Task 10/src/CountDigitRecursive.java
@@ -1,34 +1,30 @@
 public class CountDigitRecursive {
+
     public static int countDigit(int number, int digit) {
-        // Special case: if the number is 0
         if (number == 0) {
             return (digit == 0) ? 1 : 0;
+        }
 
-            // Base case
-            if (number < 10) {
-                return (number == digit) ? 1 : 0;
-            }
+        if (number < 10) {
+            return (number == digit) ? 1 : 0;
+        }
 
-            // Check the last digit and recurse on the remaining digits
-            if (number % 10 == digit) {
-                return 1 + countDigit(number / 10, digit);
-            } else {
-                return countDigit(number / 10, digit);
-            }
-        }}
+        if (number % 10 == digit) {
+            return 1 + countDigit(number / 10, digit);
+        } else {
+            return countDigit(number / 10, digit);
+        }
+    }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
-            IO.println("=== Count Occurrences of a Digit in a Number ===");
+        IO.println("=== Count Occurrences of a Digit in a Number ===");
 
-        int number = IO.readInt("Enter a number: ");
-        int digit = IO.readInt("Enter the digit to count: ");
+        int number = Integer.parseInt(IO.readln("Enter a number: "));
+        int digit = Integer.parseInt(IO.readln("Enter the digit to count: "));
 
         int result = countDigit(number, digit);
 
         IO.println("The digit " + digit + " appears " + result + " time(s) in the number.");
-
     }
-
-    }
-
+}

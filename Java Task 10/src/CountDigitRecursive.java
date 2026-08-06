10db74ed40475b1e180ace42c1f87b86a03cca81
mohammed <mooodo150@gmail.com>
2026-08-06 08:54:20 +0400

    I Create Special case: if the number is 0

--- Java Task 10/src/CountDigitRecursive.java
+++ Java Task 10/src/CountDigitRecursive.java
@@ -1,6 +1,8 @@
 public class CountDigitRecursive {
     public static int countDigit(int number, int digit){
-
+        // Special case: if the number is 0
+        if (number == 0) {
+            return (digit == 0) ? 1 : 0;
 
     }
 

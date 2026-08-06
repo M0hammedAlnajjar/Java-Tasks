6f576cbafd66f80f292c0f36666e5b904ab33497
mohammed <mooodo150@gmail.com>
2026-08-06 09:56:28 +0400

    I Create Base case: last element reached

--- Java Task 10/src/CheckArraySorted.java
+++ Java Task 10/src/CheckArraySorted.java
@@ -1,7 +1,9 @@
 public class CheckArraySorted {
 
     public static boolean isSorted(int[] arr, int index) {
-
+        // Base case: last element reached
+        if (index >= arr.length - 1) {
+            return true;
 
     }
     static void main(String[] args) {

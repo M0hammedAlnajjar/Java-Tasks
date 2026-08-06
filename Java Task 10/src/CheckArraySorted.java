1a85525dd39c6f9dbb9dd1f21bb57630ac3ddcac
mohammed <mooodo150@gmail.com>
2026-08-06 10:56:31 +0400

    I Create a Java class named CheckSortedArray

--- Java Task 10/src/CheckArraySorted.java
+++ Java Task 10/src/CheckArraySorted.java
@@ -1,26 +1,33 @@
 public class CheckArraySorted {
 
     public static boolean isSorted(int[] arr, int index) {
-        // Base case: last element reached
+        // Base case
         if (index >= arr.length - 1) {
             return true;
-
-            // If current element is greater than the next, array is not sorted
-            if (arr[index] > arr[index + 1]) {
-                return false;
-            }
-            // Check the rest of the array
-            return isSorted(arr, index + 1);
-    }
-    static void main(String[] args) {
-
-
-            IO.println("=== Check if an Array is Sorted ===");
-
-
-            int size = Integer.parseInt(IO.readln("Enter the number of elements: "));
-            int[] arr = new int[size];
         }
 
+        // If current element is greater than the next, array is not sorted
+        if (arr[index] > arr[index + 1]) {
+            return false;
+        }
+
+        // Recursive call
+        return isSorted(arr, index + 1);
+    }
+
+    public static void main(String[] args) {
+
+        IO.println("=== Check if an Array is Sorted ===");
+
+        int size = Integer.parseInt(IO.readln("Enter the number of elements: "));
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(IO.readln("Enter element " + (i + 1) + ": "));
+        }
+
+        boolean result = isSorted(arr, 0);
+
+        IO.println("Is the array sorted? " + result);
     }
 }

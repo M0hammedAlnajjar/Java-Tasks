8dbec2892cad9fb8726f9e4e3117a6f24dd456e9
mohammed <mooodo150@gmail.com>
2026-08-09 12:23:04 +0400

    I Create Get the remainder when dividing by 2

--- Java Task 10/src/ToBinary.java
+++ Java Task 10/src/ToBinary.java
@@ -1,7 +1,7 @@
 public class ToBinary {
     static String toBinary(int number) {
         if (number == 0 || number == 1) { return String.valueOf(number); }    }
-
+    int remainder = number % 2;
     static void main(String[] args) {
 
     }

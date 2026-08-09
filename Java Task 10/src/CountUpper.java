4fe00eccd86024b99633bb1c5d70431a87d701ed
mohammed <mooodo150@gmail.com>
2026-08-09 13:54:02 +0400

    I Create Add the count from the first character to the rest of the string

--- Java Task 10/src/CountUpper.java
+++ Java Task 10/src/CountUpper.java
@@ -4,7 +4,11 @@ public class CountUpper {
 
             return 0;
         }
-        int count = 0; if (Character.isUpperCase(text.charAt(0))) { count = 1; }
+        int count = 0;
+        if (Character.isUpperCase(text.charAt(0))) {
+            count = 1;
+        }
+        return count + countUpper(text.substring(1));
     }
 
     static void main(String[] args) {

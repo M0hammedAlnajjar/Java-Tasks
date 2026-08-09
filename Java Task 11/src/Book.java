476482c2191a77be27dc018220cb4aefbc79ef0d
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-09 23:42:01 +0400

    I mode validation

--- Java Task 11/src/Book.java
+++ Java Task 11/src/Book.java
@@ -1,51 +1,58 @@
-public class Book implements LibraryItem{
+public class Book implements LibraryItem {
 
     private String title;
     private String author;
     private int pages;
-
 
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
-
-            if (title == null || title.trim().isEmpty()) {
-                System.out.println("Title cannot be empty.");
-                return;
-            }
-
+        if (title != null && !title.trim().isEmpty()) {
             this.title = title;
-
-        this.title = title;
+        } else {
+            System.out.println("Name cannot be empty");
+        }
     }
 
-    @Override
-    public String getLabel() {
-
-
-        return "";
+    public String getAuthor() {
+        return author;
     }
 
-    @Override
-    public String getShelfCode() {
-        return "";
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        if (pages > 0) {
+            this.pages = pages;
+        } else {
+            this.pages = 0;
+            System.out.println("Pages must be above 0");
+        }
     }
 
     @Override
     public void printAllInfo() {
-
+        System.out.println(
+                "Book: " + title +
+                        " by " + author +
+                        " (" + pages + " pages)"
+        );
     }
 
-    public void setPages(int pages) {
-        if (pages <= 0) {
-            System.out.println("Pages must be above 0.");
-            this.pages = 0;
-            return;
-        }
+    @Override
+    public String getLabel() {
+        return "Book: " + title + " by " + author;
+    }
 
-        this.pages = pages;
+    @Override
+    public String getShelfCode() {
+        return "B-01";
     }
 }
-

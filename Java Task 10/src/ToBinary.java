726a4400f641df073ea078e5e1a048501367cfe3
mohammed <mooodo150@gmail.com>
2026-08-09 12:22:22 +0400

    I Create Base case: if the number is 0 or 1, return it as a string

--- Java Task 10/src/ToBinary.java
+++ Java Task 10/src/ToBinary.java
@@ -1,6 +1,6 @@
 public class ToBinary {
     static String toBinary(int number) {
-    }
+        if (number == 0 || number == 1) { return String.valueOf(number); }    }
 
     static void main(String[] args) {
 

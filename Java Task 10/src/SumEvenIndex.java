c0ea67dcfcb309abdf8d3e518db51a23a5b01e27
mohammed <mooodo150@gmail.com>
2026-08-09 12:08:23 +0400

    I Create Display the program title

--- Java Task 10/src/SumEvenIndex.java
+++ Java Task 10/src/SumEvenIndex.java
@@ -2,9 +2,14 @@ public class SumEvenIndex {
     static int sumEvenIndex(int[] numbers, int index) {
 
         if (index >= numbers.length) { return 0; }
+
+        return numbers[index] + sumEvenIndex(numbers, index + 2);
+
     }
 
     static void main(String[] args) {
 
+
+        IO.println("=== Sum of Array Elements at Even Indexes ===");
     }
 }

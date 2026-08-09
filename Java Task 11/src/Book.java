14048d2ac618f4f443c0e181c7655008244fe1bd
mohammed <mooodo150@gmail.com>
2026-08-09 14:40:11 +0400

    I create The three fields

--- Java Task 11/src/Book.java
+++ Java Task 11/src/Book.java
@@ -1,3 +1,22 @@
-public class Book {
+public class Book implements LibraryItem{
 
+    private String title;
+    private String author;
+    private int pages;;
+
+    @Override
+    public String getTitle() {
+        return "";
+    }
+
+    @Override
+    public int getPages() {
+        return 0;
+    }
+
+    @Override
+    public void printAllInfo() {
+
+    }
 }
+

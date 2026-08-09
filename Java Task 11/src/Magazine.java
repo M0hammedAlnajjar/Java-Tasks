c56529fe8accd68d268ddbb4f80f65634802e6f7
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-09 23:58:10 +0400

    I create getter and setter methods

--- Java Task 11/src/Magazine.java
+++ Java Task 11/src/Magazine.java
@@ -5,6 +5,21 @@ public class Magazine implements LibraryItem {
     private int issueNumber;
     private String month;
 
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(int issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
 
     @Override
     public String getLabel() {

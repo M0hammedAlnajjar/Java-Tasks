6ee8ab1c0e8615feb9f07677cdd144ecfcb987cf
mohammed <mooodo150@gmail.com>
2026-08-09 13:53:00 +0400

    I Create Check if the first character is uppercase

--- Java Task 10/src/CountUpper.java
+++ Java Task 10/src/CountUpper.java
@@ -1,6 +1,10 @@
 public class CountUpper {
     static int countUpper(String text) {
+        if (text.length() == 0) {
 
+            return 0;
+        }
+        int count = 0; if (Character.isUpperCase(text.charAt(0))) { count = 1; }
     }
 
     static void main(String[] args) {

1ef03d9a121cbc0f5a963b26ce0f6cb06cb3a2f4
mohammed <mooodo150@gmail.com>
2026-08-09 12:26:14 +0400

    I Create Convert the input from String to int

--- Java Task 10/src/ToBinary.java
+++ Java Task 10/src/ToBinary.java
@@ -13,7 +13,7 @@ public class ToBinary {
 
         IO.println("=== Convert a Number to Binary ===");
         String numberInput = IO.readln("Enter a number: ");
-
+        int number = Integer.parseInt(numberInput);
 
 
     }

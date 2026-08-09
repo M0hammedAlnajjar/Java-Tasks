a5c41e803393f3fbaf99b01470915bba8fdac48b
mohammed <mooodo150@gmail.com>
2026-08-09 12:02:10 +0400

    I Create Base case: if the index is past the end of the array

--- Java Task 10/src/SumEvenIndex.java
+++ Java Task 10/src/SumEvenIndex.java
@@ -1,7 +1,7 @@
 public class SumEvenIndex {
     static int sumEvenIndex(int[] numbers, int index) {
 
-
+        if (index >= numbers.length) { return 0; }
     }
 
     static void main(String[] args) {

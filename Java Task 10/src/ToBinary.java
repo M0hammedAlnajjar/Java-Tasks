4b9ce95749ccb0d343dbffe257dad0d732e16426
mohammed <mooodo150@gmail.com>
2026-08-09 12:25:13 +0400

    I Create Display the program title

--- Java Task 10/src/ToBinary.java
+++ Java Task 10/src/ToBinary.java
@@ -1,10 +1,18 @@
 public class ToBinary {
     static String toBinary(int number) {
-        if (number == 0 || number == 1) { return String.valueOf(number); }    }
-    int remainder = number % 2;
+        if (number == 0 || number == 1) {
+            return String.valueOf(number);
+        }
+        int remainder = number % 2;
 
-    return toBinary(number / 2) + remainder;
+        return toBinary(number / 2) + remainder;
+    }
+
+
     static void main(String[] args) {
+
+        IO.println("=== Convert a Number to Binary ===");
+
 
     }
 }

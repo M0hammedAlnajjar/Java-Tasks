fb35e6e98b0b75145758208557c619f40b8889b3
mohammed <mooodo150@gmail.com>
2026-08-10 12:30:07 +0400

    I create generate  booked patient getters and setters

--- Java Task 11/src/ClinicWaitingList/BookedPatient.java
+++ Java Task 11/src/ClinicWaitingList/BookedPatient.java
@@ -6,5 +6,27 @@ public class BookedPatient {
     private int age ;
     private double slotTime;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSlotTime() {
+        return slotTime;
+    }
+
+    public void setSlotTime(double slotTime) {
+        this.slotTime = slotTime;
+    }
 }

03788ab97ce18dc62d4c8b466ee1d9e347c06f88
mohammed <mooodo150@gmail.com>
2026-08-10 16:09:51 +0400

    I create import array list ,list and declare list after that add that first patient

--- Java Task 11/src/ClinicWaitingList/ClinicWaitingList.java
+++ Java Task 11/src/ClinicWaitingList/ClinicWaitingList.java
@@ -1,4 +1,19 @@
 package ClinicWaitingList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClinicWaitingList {
+
+
+    static void main(String[] args) {
+        new ClinicWaitingList().run();
+
+
+    }
+
+    private void run() {
+        List<Attendable> queue = new ArrayList<>();
+
+    }
 }

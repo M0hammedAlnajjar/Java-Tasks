8e79ec9f751849477bb385e89de3f51494e3af7c
mohammed <mooodo150@gmail.com>
2026-08-10 09:57:32 +0400

    I create generate getters and setters in service

--- Java Task 11/src/CornerShopInventory/Service.java
+++ Java Task 11/src/CornerShopInventory/Service.java
@@ -4,4 +4,29 @@ public class Service {
     private String name;
     private double price;
     private double minutes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(double minutes) {
+        this.minutes = minutes;
+    }
 }
+

d4a28cd8421d5de3d86f8a54c60fef67af57ff7f
mohammed <mooodo150@gmail.com>
2026-08-10 14:19:45 +0400

    I create generate walk in visitor getters and setters

--- Java Task 11/src/ClinicWaitingList/WalkInVisitor.java
+++ Java Task 11/src/ClinicWaitingList/WalkInVisitor.java
@@ -10,4 +10,31 @@ public class WalkInVisitor {
     private double arrivalOder;
 
 
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+
+
+    }
+
+    public double getArrivalOder() {
+        return arrivalOder;
+    }
+
+    public void setArrivalOder(double arrivalOder) {
+        this.arrivalOder = arrivalOder;
+    }
 }

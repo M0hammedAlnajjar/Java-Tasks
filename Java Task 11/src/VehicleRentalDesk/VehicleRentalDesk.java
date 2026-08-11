533a77157e6f1d93e391419b6d24146c26ad0e12
mohammed <mooodo150@gmail.com>
2026-08-11 09:09:48 +0400

    I import array list and list also calling

--- Java Task 11/src/VehicleRentalDesk/VehicleRentalDesk.java
+++ Java Task 11/src/VehicleRentalDesk/VehicleRentalDesk.java
@@ -1,4 +1,18 @@
 package VehicleRentalDesk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleRentalDesk {
+
+    static void main(String[] args) {
+
+        new VehicleRentalDesk().run();
+        
+
+    }
+    List<Rentable> vehicles = new ArrayList<>();
+
+    private void run() {
+    }
 }

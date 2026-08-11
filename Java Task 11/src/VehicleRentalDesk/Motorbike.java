b4efb65cfbe023e25b46ef016c91f47914b7b597
mohammed <mooodo150@gmail.com>
2026-08-11 08:55:14 +0400

    I modify four field and mark private in class motorbike

--- Java Task 11/src/VehicleRentalDesk/Motorbike.java
+++ Java Task 11/src/VehicleRentalDesk/Motorbike.java
@@ -1,4 +1,10 @@
 package VehicleRentalDesk;
 
 public class Motorbike {
+
+    private String plateNumber;
+    private double dailyRate;
+    private int rentalDays;
+    private int engineSize;
+
 }

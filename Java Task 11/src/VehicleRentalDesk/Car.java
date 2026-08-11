e31368a0be1eafb6ae7bfe0bbcc4e490cf932341
mohammed <mooodo150@gmail.com>
2026-08-11 07:57:27 +0400

    I create generate  getters and setters in car class

--- Java Task 11/src/VehicleRentalDesk/Car.java
+++ Java Task 11/src/VehicleRentalDesk/Car.java
@@ -7,5 +7,35 @@ public class Car {
     private int rentalDays;
     private int seats;
 
+    public String getPlateNumber() {
+        return plateNumber;
+    }
 
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public double getDailyRate() {
+        return dailyRate;
+    }
+
+    public void setDailyRate(double dailyRate) {
+        this.dailyRate = dailyRate;
+    }
+
+    public int getRentalDays() {
+        return rentalDays;
+    }
+
+    public void setRentalDays(int rentalDays) {
+        this.rentalDays = rentalDays;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
 }

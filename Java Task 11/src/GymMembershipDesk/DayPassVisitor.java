bc7d7156babbfa45c9c710b0f73c11e71f19b774
mohammed <mooodo150@gmail.com>
2026-08-11 12:04:26 +0400

    I mode display monthy total

--- Java Task 11/src/GymMembershipDesk/DayPassVisitor.java
+++ Java Task 11/src/GymMembershipDesk/DayPassVisitor.java
@@ -2,8 +2,33 @@ package GymMembershipDesk;
 
 public class DayPassVisitor implements Payable {
 
-    private  String name;
+    private String name;
     private  int age;
+    private double monthlyFee;
+    private final int membershipId;
+
+    public double getMonthlyFee() {
+        return monthlyFee;
+    }
+
+    public void setMonthlyFee(double monthlyFee) {
+
+        if (monthlyFee > 0
+                && monthlyFee <= 500) {
+
+            this.monthlyFee = monthlyFee;
+
+        } else {
+
+            System.out.println(
+                    "Monthly fee must be above 0 and not more than 500"
+            );
+    }
+    }
+
+    public int getMembershipId() {
+        return membershipId;
+    }
 
     public void setName(String name) {
         if (name != null
@@ -46,12 +71,16 @@ public class DayPassVisitor implements Payable {
 
     @Override
     public void printAllInfo() {
+        IO.print("Name: " + name);
+        IO.print("Age: " + age);
+        IO.print("Monthly fee: " + monthlyFee);
+        IO.print("Membership Id: " + membershipId);
 
     }
 
     @Override
     public double monthlyTotal() {
-        return 0;
+        return MonthlyMember;
     }
 
     @Override

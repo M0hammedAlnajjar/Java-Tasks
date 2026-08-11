ab93aaeac3cd1bbabd5c18330e4df28dcb3d2ee0
mohammed <mooodo150@gmail.com>
2026-08-11 11:57:29 +0400

    I mode validation in set name

--- Java Task 11/src/GymMembershipDesk/DayPassVisitor.java
+++ Java Task 11/src/GymMembershipDesk/DayPassVisitor.java
@@ -2,6 +2,30 @@ package GymMembershipDesk;
 
 public class DayPassVisitor implements Payable {
 
+    private  String name;
+    private  int age;
+
+    public void setName(String name) {
+        if (name != null
+                && name.trim().length() >= 2) {
+
+            this.name = name;
+
+        } else {
+
+            System.out.println(
+                    "Member name must be at least 2 characters"
+            );
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     @Override
     public void printAllInfo() {

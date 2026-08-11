610bd28050808bb2a9ea750efcd6bb7511c5784c
mohammed <mooodo150@gmail.com>
2026-08-11 11:42:16 +0400

    I use implemwnting in classs day pass visitor

--- Java Task 11/src/GymMembershipDesk/DayPassVisitor.java
+++ Java Task 11/src/GymMembershipDesk/DayPassVisitor.java
@@ -1,4 +1,20 @@
 package GymMembershipDesk;
 
-public class DayPassVisitor {
+public class DayPassVisitor implements Payable {
+
+
+    @Override
+    public void printAllInfo() {
+
+    }
+
+    @Override
+    public double monthlyTotal() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
 }

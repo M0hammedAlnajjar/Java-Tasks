83ea3c476684f2c2b2094b2265abd0f6163e8851
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-11 22:44:48 +0400

    Sets the first book author

--- Java Task 12/src/YourFirstClass/Main.java
+++ Java Task 12/src/YourFirstClass/Main.java
@@ -4,7 +4,8 @@ public class Main {
     static void main(String[] args) {
         Book b = new Book();
         b.title = "Java Basics ";
-        b.author = " Maryam ";
+//      b.author = " Maryam ";
+        b.author = "Ishaq";
 
         b.printInfo();
     }
